5241181c960a4e0c396ee7bbaf3416cd37710087
WhiteCodaInc <notifications@whitecoda.com>
2015-05-12 10:32:58 +0530

    update

--- application/views/dashboard/add-contact.php
+++ application/views/dashboard/add-contact.php
@@ -120,7 +120,7 @@
     $(document).ready(function () {
         console.log(hopscotch.getState());
         if (hopscotch.getState() == "welcome:9:7") {
-            hopscotch.nextStep();
+            hopscotch.showStep(10);
         }
         $('#save-contact').click(function () {
             var tourStep = hopscotch.getState();
