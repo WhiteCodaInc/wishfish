50ee24c43073fc741500ff0517e6c8784e66dc32
WhiteCodaInc <notifications@whitecoda.com>
2015-05-12 13:35:38 +0530

    update

--- application/views/dashboard/upgrade.php
+++ application/views/dashboard/upgrade.php
@@ -8,7 +8,7 @@
     <!-- Main content -->
     <section class="content">
         <div class="row">
-            <div class="col-md-2"></div>
+            <div class="col-md-1"></div>
             <div class="col-md-3">
                 <div class="box box-solid">
                     <div class="box-header" style="text-align: center">
