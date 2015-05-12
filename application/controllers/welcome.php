0f64808c6626a5aae7f9d32bbd95710bd319f2ca
WhiteCodaInc <notifications@whitecoda.com>
2015-05-12 16:18:50 +0530

    update

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -23,9 +23,27 @@ class Welcome extends CI_Controller {
     public function index() {
         $this->load->library('encryption');
 
-        echo 'BUCKET : ' . $this->encryption->encode("mikhailkuznetsov") . '<br>';
-        echo 'ACCESS KEY : ' . $this->encryption->encode("AKIAJTSSWQFVK3YRSDNQ") . '<br>';
-        echo 'SECRET KEY : ' . $this->encryption->encode("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") . '<br>';
+        $bucket = "mikhailkuznetsov";
+        $access = "AKIAJTSSWQFVK3YRSDNQ";
+        $secret = "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";
+
+        echo '-------------Original--------------<br>';
+
+        echo $bucket . '<br>';
+        echo $access . '<br>';
+        echo $secret . '<br>';
+
+        echo '-------------Encryption--------------<br>';
+
+        echo 'BUCKET : ' . $this->encryption->encode($bucket) . '<br>';
+        echo 'ACCESS KEY : ' . $this->encryption->encode($access) . '<br>';
+        echo 'SECRET KEY : ' . $this->encryption->encode($secret) . '<br>';
+
+        echo '-------------Decryption--------------<br>';
+
+        echo 'BUCKET : ' . $this->encryption->encode("ShLvoZAkeBrawkThnA-akEfhitL4Vcgr-oBnwAn-bUU") . '<br>';
+        echo 'ACCESS KEY : ' . $this->encryption->encode("kjruBRpR5XOSbsqgGLeRMJwFasiaIUrZsNaqjc_LLic") . '<br>';
+        echo 'SECRET KEY : ' . $this->encryption->encode("jN86kD3vVS_wSPV0HOpfkr8UtTIQ2BbdX85scQqywnG82fMIY9aPlxHtsFTvUJiSzD3IjjSg2F6Re_ZUbiUJmg") . '<br>';
 
         //$this->load->view('welcome_message');
     }
