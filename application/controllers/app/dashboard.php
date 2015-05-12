98a04cd9a11816145cf47bde70887ec9296f7cab
WhiteCodaInc <notifications@whitecoda.com>
2015-05-12 09:10:40 +0530

    update

--- application/controllers/app/dashboard.php
+++ application/controllers/app/dashboard.php
@@ -140,21 +140,21 @@ class Dashboard extends CI_Controller {
         $phone = (preg_match('/^\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/', $set['phone'])) ?
                 str_replace(array('(', ')', ' ', '-'), '', $set['code'] . $set['phone']) :
                 NULL;
-        echo $phone;
-        die();
+
         if ($phone) {
-            $code = $this->common->getRandomDigit(6);
-            $str = "Hey,This is Wish-Fish ! Your Verification code is:{$code}";
-            if ($this->common->sendSMS($phone, $str)) {
-                $userinfo = $this->common->getUserInfo($userid);
-                if ($userinfo->phone == NULL) {
-                    $this->db->update('user_mst', array('phone' => $phone), array('user_id' => $userid));
-                }
-                $this->session->set_userdata('verificationCode', $code);
-                echo 1;
-            } else {
-                echo 0;
-            }
+//            $code = $this->common->getRandomDigit(6);
+//            $str = "Hey,This is Wish-Fish ! Your Verification code is:{$code}";
+//            if ($this->common->sendSMS($phone, $str)) {
+//                $userinfo = $this->common->getUserInfo($userid);
+//                if ($userinfo->phone == NULL) {
+//                    $this->db->update('user_mst', array('phone' => $phone), array('user_id' => $userid));
+//                }
+//                $this->session->set_userdata('verificationCode', $code);
+//                echo 1;
+//            } else {
+//                echo 0;
+//            }
+            echo 1;
         } else {
             echo 0;
         }
