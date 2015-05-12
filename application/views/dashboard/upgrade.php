2a2d3325ae7fac9b81410e4d01613f58a4d3639d
WhiteCodaInc <notifications@whitecoda.com>
2015-05-12 15:02:50 +0530

    update

--- application/views/dashboard/upgrade.php
+++ application/views/dashboard/upgrade.php
@@ -75,6 +75,7 @@
                             Upgrade
                         </button>
                     </div><!-- /.box-body -->
+                    <div class="overlay"></div>
                 </div>
             </div>
             <div class="col-md-4">
@@ -100,48 +101,50 @@
             </div>
             <div class="col-md-2"></div>
         </div>
-        <form style="display: none" id="personal" action="<?= site_url() ?>app/upgrade/pay" method="post">
-            <input type="hidden" name="amount" value="<?= $pdetail[0]->amount ?>"/>
-            <input type="hidden" name="frequency" value="1"/>
-            <input type="hidden" name="name" value="<?= $pdetail[0]->plan_name ?>"/>
-            <input type="hidden" name="id" value="<?= $pdetail[0]->plan_id ?>"/>
+        <?php if (!$card): ?>
+            <form style="display: none" id="personal" action="<?= site_url() ?>app/upgrade/pay" method="post">
+                <input type="hidden" name="amount" value="<?= $pdetail[0]->amount ?>"/>
+                <input type="hidden" name="frequency" value="1"/>
+                <input type="hidden" name="name" value="<?= $pdetail[0]->plan_name ?>"/>
+                <input type="hidden" name="id" value="<?= $pdetail[0]->plan_id ?>"/>
 
-            <script
-                src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                data-key="<?= $gatewayInfo->publish_key ?>"
-                data-image="/square-image.png"
-                data-name="<?= $pdetail[0]->plan_name ?>"
-                data-description="Product"                    
-                data-label="Stripe"                    
-                >
-            </script>
-        </form>
-<!--        <form style="display: none" id="enterprise" action="<?= site_url() ?>app/upgrade/pay" method="post">
-        <input type="hidden" name="amount" value="<?= $pdetail[1]->amount ?>"/>
-        <input type="hidden" name="frequency" value="1"/>
-        <input type="hidden" name="name" value="<?= $pdetail[1]->plan_name ?>"/>
-        <input type="hidden" name="id" value="<?= $pdetail[1]->plan_id ?>"/>
+                <script
+                    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                    data-key="<?= $gatewayInfo->publish_key ?>"
+                    data-image="/square-image.png"
+                    data-name="<?= $pdetail[0]->plan_name ?>"
+                    data-description="Product"                    
+                    data-label="Stripe"                    
+                    >
+                </script>
+            </form>
+            <form style="display: none" id="enterprise" action="<?= site_url() ?>app/upgrade/pay" method="post">
+                <input type="hidden" name="amount" value="<?= $pdetail[1]->amount ?>"/>
+                <input type="hidden" name="frequency" value="1"/>
+                <input type="hidden" name="name" value="<?= $pdetail[1]->plan_name ?>"/>
+                <input type="hidden" name="id" value="<?= $pdetail[1]->plan_id ?>"/>
 
-        <script
-            src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-            data-key="<?= $gatewayInfo->publish_key ?>"
-            data-image="/square-image.png"
-            data-name="<?= $pdetail[1]->plan_name ?>"
-            data-description="Product"                    
-            data-label="Stripe"                    
-            >
+                <script
+                    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                    data-key="<?= $gatewayInfo->publish_key ?>"
+                    data-image="/square-image.png"
+                    data-name="<?= $pdetail[1]->plan_name ?>"
+                    data-description="Product"                    
+                    data-label="Stripe"                    
+                    >
+                </script>
+            </form>
+        <?php endif; ?>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $('#a_personal').click(function () {
+                    $('#personal button').trigger('click');
+                });
+                $('#a_enterprise').click(function () {
+                    $('#enterprise button').trigger('click');
+                });
+            });
         </script>
-    </form>-->
-<!--        <script type="text/javascript">
-$(document).ready(function () {
-$('#a_personal').click(function () {
-$('#personal button').trigger('click');
-});
-$('#a_enterprise').click(function () {
-$('#enterprise button').trigger('click');
-});
-});
-</script>-->
     </section>
 </aside>
 </div>
