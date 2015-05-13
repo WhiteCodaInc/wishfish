c9168680f82e98365071d8a3c7fe0fe2a534e4bc
WhiteCodaInc <notifications@whitecoda.com>
2015-05-13 17:34:11 +0530

    update

--- application/controllers/app/upgrade.php
+++ application/controllers/app/upgrade.php
@@ -133,7 +133,7 @@ class Upgrade extends CI_Controller {
     function isAllowToDowngrade() {
         $planInfo = $this->common->getPlan(2);
         $tcontacts = $this->common->getTotal($this->userid, 'contact_detail');
-        if ($planInfo->contacts > $tcontacts) {
+        if ($planInfo->contacts == "-1" || $planInfo->contacts > $tcontacts) {
             echo 1;
         } else {
             echo 0;
