f246ad2aef31d757abb154d5b74087c981df8462
WhiteCodaInc <notifications@whitecoda.com>
2015-05-13 14:16:06 +0530

    update

--- application/models/dashboard/m_calender.php
+++ application/models/dashboard/m_calender.php
@@ -284,7 +284,7 @@ class M_calender extends CI_Model {
 
     function checkTotalGroupEvent($groupid) {
         $this->db->select('count(event_id) as total');
-        $query = $this->db->get_where('schedule', array('user_id' => 200, 'group_id' => $groupid));
+        $query = $this->db->get_where('schedule', array('user_id' => $this->userid, 'group_id' => $groupid));
         return ($query->num_rows() == 1) ? $query->row() : FALSE;
     }
 
