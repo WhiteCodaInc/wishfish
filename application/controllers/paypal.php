093f33d9565564e7661a1e03deb07a5b6a2cdf35
WhiteCodaInc <notifications@whitecoda.com>
2015-05-15 17:30:53 +0530

    update

--- application/controllers/paypal.php
+++ application/controllers/paypal.php
@@ -8,11 +8,12 @@ class Paypal extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->library('paypal_lib');
+        $this->load->model('m_login', 'objlogin');
     }
 
     function index() {
         $post = $this->input->post();
-        $this->session->set_userdata($post);
+        $this->session->set_flashdata($post);
         $gatewayInfo = $this->common->getPaymentGatewayInfo("PAYPAL");
         $this->paypal_lib->set_acct_info($gatewayInfo->api_username, $gatewayInfo->api_password, $gatewayInfo->api_signature);
 
@@ -40,47 +41,118 @@ class Paypal extends CI_Controller {
     }
 
     function consolidate() {
-
         echo '<pre>';
-        print_r($this->session->all_userdata());
+        print_r($this->session->flashdata('item_name'));
         die();
-
         if ($this->input->get('token')) { // Token parameter exists
             $gatewayInfo = $this->common->getPaymentGatewayInfo("PAYPAL");
             $this->paypal_lib->set_acct_info(
                     $gatewayInfo->api_username, $gatewayInfo->api_password, $gatewayInfo->signature
             );
-
+            $this->db->trans_start();
             $checkoutDetails = $this->paypal_lib->request('GetExpressCheckoutDetails', array('TOKEN' => $this->input->get('token')));
+            $uid = $this->insertUser($checkoutDetails);
+
+            $planid = ($this->session->userdata('item_name') == "wishfish-personal") ? 2 : 3;
 
             // Complete the checkout transaction
             $requestParams = array(
                 'TOKEN' => $this->input->get('token'),
                 'PROFILESTARTDATE' => $checkoutDetails['TIMESTAMP'],
-                'DESC' => 'Recurring Profile',
+                'DESC' => $this->session->userdata('item_name'),
                 'BILLINGPERIOD' => 'Month',
+                'PROFILEREFERENCE' => $uid,
                 'BILLINGFREQUENCY' => 1,
                 'TOTALBILLINGCYCLES' => 0,
-                'AMT' => 9.99,
-                'INITAMT' => 9.99,
+                'AMT' => $this->session->userdata('amount'),
                 'CURRENCYCODE' => 'USD',
                 'MAXFAILEDPAYMENTS' => 3,
                 'FAILEDINITAMTACTION' => 'CancelOnFailure'
             );
 
             $response = $this->paypal_lib->request('CreateRecurringPaymentsProfile', $requestParams);
-            if (is_array($response) && $response['ACK'] == 'Success') { // Payment successful
-                //$this->objrecurring->expressCO($set['recurid']);
-                $this->session->set_flashdata('msg', "success");
+            if (is_array($response) && $response['ACK'] == 'Success') {
+                $this->insertPlanDetail($uid, $planid, $response);
+                $this->db->trans_complete();
+                $this->sendMail($checkoutDetails, $uid);
+                header('Location:' . site_url() . 'login');
             } else {
-                $this->session->set_flashdata('error', $response['ACK']);
+                header('Location:' . site_url() . 'home');
             }
-            header('Location:' . site_url() . 'app/profile/expressCO');
         }
+    }
+
+    function insertUser($data) {
+        $user_set = array(
+            'email' => $data['EMAIL'],
+            'password' => $this->generateRandomString(5),
+            'customer_id' => $data['PAYERID'],
+            'gateway' => "PAYPAL",
+            'is_set' => 1,
+            'register_date' => $data['TIMESTAMP']
+        );
+        $this->db->insert('user_mst', $user_set);
+        return $this->db->insert_id();
+    }
+
+    function insertPlanDetail($userid, $planid, $data) {
+
+        $start_dt = date('Y-m-d', $data['TIMESTAMP']);
+        $expiry_date = $this->common->getNextDate($start_dt, '1 months');
+
+        $amount = $data['AMT'];
+        $planInfo = $this->common->getPlan($planid);
+        $plan_set = array(
+            'user_id' => $userid,
+            'plan_id' => $planid,
+            'contacts' => $planInfo->contacts,
+            'sms_events' => $planInfo->sms_events,
+            'email_events' => $planInfo->email_events,
+            'group_events' => $planInfo->group_events,
+            'amount' => $amount,
+            'plan_status' => 1,
+            'start_date' => $start_dt,
+            'expiry_date' => $expiry_date
+        );
+        $this->db->insert('plan_detail', $plan_set);
+        return true;
+    }
+
+    function generateRandomString($length = 5) {
+        return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
     }
 
     function cancelled() {
         header('location:' . site_url() . 'client/client_recurring');
+    }
+
+    function sendMail($post, $userid) {
+        $uid = $this->encryption->encode($userid);
+        $templateInfo = $this->common->getAutomailTemplate("NEW USER REGISTRATION");
+        $url = site_url() . 'app/dashboard?uid=' . $uid;
+        $link = "<table border='0' align='center' cellpadding='0' cellspacing='0' class='mainBtn' style='margin-top: 0;margin-left: auto;margin-right: auto;margin-bottom: 0;padding-top: 0;padding-bottom: 0;padding-left: 0;padding-right: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;border-collapse: collapse;border-spacing: 0;'>";
+        $link .= "<tr>";
+        $link .= "<td align='center' valign='middle' class='btnMain' style='margin-top: 0;margin-left: 0;margin-right: 0;margin-bottom: 0;padding-top: 12px;padding-bottom: 12px;padding-left: 22px;padding-right: 22px;border-collapse: collapse;border-spacing: 0;-webkit-text-size-adjust: none;font-family: Arial, Helvetica, sans-serif;background-color: {$templateInfo['color']};height: 20px;font-size: 18px;line-height: 20px;mso-line-height-rule: exactly;text-align: center;vertical-align: middle;'>
+                                            <a href='{$url}' style='padding-top: 0;padding-bottom: 0;padding-left: 0;padding-right: 0;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;font-family: Arial, Helvetica, sans-serif;color: #ffffff;font-weight: bold;'>
+                                                <span style='text-decoration: none;color: #ffffff;'>
+                                                    Active Your Account
+                                                </span>
+                                            </a>
+                                        </td>";
+        $link .= "</tr></table>";
+        $tag = array(
+            'NAME' => "User",
+            'LINK' => $link,
+            'THISDOMAIN' => "Wish-Fish"
+        );
+        $subject = $this->parser->parse_string($templateInfo['mail_subject'], $tag, TRUE);
+        $this->load->view('email_format', $templateInfo, TRUE);
+        $body = $this->parser->parse('email_format', $tag, TRUE);
+
+        $from = ($templateInfo['from'] != "") ? $templateInfo['from'] : NULL;
+        $name = ($templateInfo['name'] != "") ? $templateInfo['name'] : NULL;
+
+        return $this->common->sendAutoMail($post['EMAIL'], $subject, $body, $from, $name);
     }
 
 }
