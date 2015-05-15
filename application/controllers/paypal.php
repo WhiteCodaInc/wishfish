17bf39ad117157bf4ff9e0d0013971b09a5b4ee5
WhiteCodaInc <notifications@whitecoda.com>
2015-05-15 16:24:01 +0530

    update

--- application/controllers/paypal.php
+++ application/controllers/paypal.php
@@ -15,7 +15,7 @@ class Paypal extends CI_Controller {
     function index() {
         $this->post = $this->input->post();
         $gatewayInfo = $this->common->getPaymentGatewayInfo("EC");
-        $this->paypal->set_acct_info($gatewayInfo->api_username, $gatewayInfo->api_password, $gatewayInfo->api_signature);
+        $this->paypal_lib->set_acct_info($gatewayInfo->api_username, $gatewayInfo->api_password, $gatewayInfo->api_signature);
 
         $requestParams = array(
             'RETURNURL' => site_url() . 'paypal/consolidate',
@@ -26,12 +26,12 @@ class Paypal extends CI_Controller {
             'L_BILLINGTYPE0' => 'RecurringPayments',
             'L_BILLINGAGREEMENTDESCRIPTION0' => $this->post['item_name']
         );
-        $response = $this->paypal->request('SetExpressCheckout', $requestParams + $recurring);
+        $response = $this->paypal_lib->request('SetExpressCheckout', $requestParams + $recurring);
 
         if (is_array($response) && $response['ACK'] == 'Success') { //Request successful
             $token = $response['TOKEN'];
 
-            if ($this->paypal->is_sandbox)
+            if ($this->paypal_lib->is_sandbox)
                 echo 'https://www.sandbox.paypal.com/webscr?cmd=_express-checkout&token=' . urlencode($token);
             else
                 echo 'https://www.paypal.com/webscr?cmd=_express-checkout&token=' . urlencode($token);
@@ -48,11 +48,11 @@ class Paypal extends CI_Controller {
 
         if ($this->input->get('token')) { // Token parameter exists
             $gatewayInfo = $this->common->getPaymentGatewayInfo("EC");
-            $this->paypal->set_acct_info(
+            $this->paypal_lib->set_acct_info(
                     $gatewayInfo->api_username, $gatewayInfo->api_password, $gatewayInfo->signature
             );
 
-            $checkoutDetails = $this->paypal->request('GetExpressCheckoutDetails', array('TOKEN' => $this->input->get('token')));
+            $checkoutDetails = $this->paypal_lib->request('GetExpressCheckoutDetails', array('TOKEN' => $this->input->get('token')));
 
             // Complete the checkout transaction
             $requestParams = array(
@@ -69,7 +69,7 @@ class Paypal extends CI_Controller {
                 'FAILEDINITAMTACTION' => 'CancelOnFailure'
             );
 
-            $response = $this->paypal->request('CreateRecurringPaymentsProfile', $requestParams);
+            $response = $this->paypal_lib->request('CreateRecurringPaymentsProfile', $requestParams);
             if (is_array($response) && $response['ACK'] == 'Success') { // Payment successful
                 //$this->objrecurring->expressCO($set['recurid']);
                 $this->session->set_flashdata('msg', "success");
