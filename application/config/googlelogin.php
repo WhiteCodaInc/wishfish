4fa4504beec33820f13d44ead5363488e38d729f
WhiteCodaInc <notifications@whitecoda.com>
2015-05-19 17:28:01 +0530

    update

--- application/config/googlelogin.php
+++ application/config/googlelogin.php
@@ -1,8 +1,8 @@
 <?php
 
 $config['googlelogin']['application_name'] = 'Wish-Fish';
-$config['googlelogin']['client_id'] = '531642364953-j7k364cgpe4h3psq3vi4gp5dellqjpmj.apps.googleusercontent.com';
-$config['googlelogin']['client_secret'] = 'JUHg4BWMAQnFD5i4GGpXGRVu';
-$config['googlelogin']['redirect_uri'] = 'https://www.wish-fish.com/login/signin';
+$config['googlelogin']['client_id'] = '531642364953-6lq0jb351ejhhq17dpk1r1rl70fgjanp.apps.googleusercontent.com';
+$config['googlelogin']['client_secret'] = 'sLi8hE6igF5vMGjopLZCZZbD';
+$config['googlelogin']['redirect_uri'] = 'https://www.wish-fish.com/login/gsignin';
 $config['googlelogin']['api_key'] = 'AIzaSyDlhC9ZCWLDiIx6p7dqaYzcDpuHDOsJ03w';
 
