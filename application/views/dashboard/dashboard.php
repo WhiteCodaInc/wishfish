fa7f9d33a43e05514772bc598fd429884a2fa36e
WhiteCodaInc <notifications@whitecoda.com>
2015-05-20 12:28:43 +0530

    update

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -247,8 +247,14 @@
                     </div><!-- /.box-header -->
                     <div class="box-body" style="text-align: center">
                         <ul>
-                            <li><i style="vertical-align: middle;font-size: 20px" class="fa fa-circle-o"></i> Schedule Unlimited Events</li>
-                            <li style="vertical-align: middle;font-size: 20px;color: green"><i class="fa fa-check-circle-o"></i> Import Contacts From Google</li>
+                            <li>
+                                <i style="vertical-align: middle;font-size: 20px" class="fa fa-circle-o"></i>
+                                Schedule Unlimited Events
+                            </li>
+                            <li style="color: green">
+                                <i style="vertical-align: middle;font-size: 20px;" class="fa fa-check-circle-o"></i>
+                                Import Contacts From Google
+                            </li>
                         </ul>
                     </div>
                 </div>
