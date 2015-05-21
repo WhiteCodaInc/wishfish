f4ad7ca8918b3ffdee4f76ad164aee353ac4e8f3
WhiteCodaInc <notifications@whitecoda.com>
2015-05-21 12:22:29 +0530

    update

--- application/controllers/app/import.php
+++ application/controllers/app/import.php
@@ -36,7 +36,7 @@ class Import extends CI_Controller {
 
     public function contacts() {
 
-        if ($this->input->get('error')) {
+        if ($this->input->get('error') != "") {
             header('location' . site_url() . 'app/dashboard');
         }
 
