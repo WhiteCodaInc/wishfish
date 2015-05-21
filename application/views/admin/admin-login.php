5ca94bfa73cc6b0f01971168364d3bbc6918bfa7
WhiteCodaInc <notifications@whitecoda.com>
2015-05-21 16:03:16 +0530

    update

--- application/views/admin/admin-login.php
+++ application/views/admin/admin-login.php
@@ -48,14 +48,14 @@
                 </div>
             </form>
 
-            <div class="margin text-center">
+<!--            <div class="margin text-center">
                 <span>Sign in using social networks</span>
                 <br/>
                 <button class="btn bg-light-blue btn-circle"><i class="fa fa-facebook"></i></button>
                 <button class="btn bg-aqua btn-circle"><i class="fa fa-twitter"></i></button>
                 <button class="btn bg-red btn-circle"><i class="fa fa-google-plus"></i></button>
 
-            </div>
+            </div>-->
         </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
