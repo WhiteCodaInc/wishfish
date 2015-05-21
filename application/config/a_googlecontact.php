d7dd89c6a6b1406fedbf681a80e8251b6338ad04
WhiteCodaInc <notifications@whitecoda.com>
2015-05-21 12:05:43 +0530

    update

--- application/config/a_googlecontact.php
+++ application/config/a_googlecontact.php
@@ -1,8 +1,8 @@
 <?php
 
-$config['googlecontact']['application_name'] = 'Contacts';
-$config['googlecontact']['client_id'] = '25105222102-i9v1vm6svak9t953o8k9ntqnjlk3relb.apps.googleusercontent.com';
-$config['googlecontact']['client_secret'] = '0fSaaeC1tp2PaBHHF7d1-fLR';
-$config['googlecontact']['redirect_uri'] = 'https://www.wish-fish.com/admin/import/contacts';
-$config['googlecontact']['api_key'] = 'AIzaSyApF2HSWkGVRSjf98S9aHhl7mxjVgi2tSY';
+$config['a_googlecontact']['application_name'] = 'Contacts';
+$config['a_googlecontact']['client_id'] = '25105222102-i9v1vm6svak9t953o8k9ntqnjlk3relb.apps.googleusercontent.com';
+$config['a_googlecontact']['client_secret'] = '0fSaaeC1tp2PaBHHF7d1-fLR';
+$config['a_googlecontact']['redirect_uri'] = 'https://www.wish-fish.com/admin/import/contacts';
+$config['a_googlecontact']['api_key'] = 'AIzaSyApF2HSWkGVRSjf98S9aHhl7mxjVgi2tSY';
 
