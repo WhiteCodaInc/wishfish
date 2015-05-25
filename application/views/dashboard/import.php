719e52cbb53e163fd342773eaf28e47deb07300d
WhiteCodaInc <notifications@whitecoda.com>
2015-05-25 11:23:14 +0530

    update

--- application/views/dashboard/import.php
+++ application/views/dashboard/import.php
@@ -102,7 +102,12 @@
                 [25, 50, 100, 200, -1],
                 [25, 50, 100, 200, "All"]
             ],
-            iDisplayLength: -1
+            aoColumnDefs: [{
+                    bSortable: false,
+                    aTargets: [0, 2, 3]
+                }],
+            iDisplayLength: -1,
+            aaSorting: [[1, 'asc']]
         });
     });
 </script>
