14cedc2faaf0734e572a034e89b440a6b34b3313
WhiteCodaInc <notifications@whitecoda.com>
2015-05-25 14:40:06 +0530

    update

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -25,12 +25,12 @@ class Dashboard extends CI_Controller {
         if (!$this->authex->logged_in()) {
             header('location:' . site_url() . 'admin/admin_login');
         } else {
-            $this->load->model('admin/m_calender', 'objcalender');
+            $this->load->model('admin/m_calender', 'objcal');
         }
     }
 
     function index() {
-        $data['card'] = $this->objcalender->getCards();
+        $data['card'] = $this->objcal->getCards();
         print_r($data);
         $this->load->view('admin/admin_header');
         $this->load->view('admin/admin_top');
