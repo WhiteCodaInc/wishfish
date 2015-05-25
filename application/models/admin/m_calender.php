0caadc79b40cdfd38a32ac784386f3f854d913d0
WhiteCodaInc <notifications@whitecoda.com>
2015-05-25 14:32:40 +0530

    update

--- application/models/admin/m_calender.php
+++ application/models/admin/m_calender.php
@@ -93,7 +93,7 @@ class M_calender extends CI_Model {
 
     function getCards() {
         $res = array();
-
+        die();
         $start = date('Y-m-01'); // hard-coded '01' for first day
         $end = date('Y-m-t');
 //        $start = ($post['start'] != "") ? gmdate("Y-m-d", $post['start']) : date('Y-m-01');
@@ -132,9 +132,6 @@ class M_calender extends CI_Model {
         $this->db->select('count(*) as totalD');
         $query = $this->db->get_where('schedule', $where);
         $res['totalD'] = $query->row()->totalD;
-        echo '<pre>';
-        print_r($res);
-        die();
         return $res;
     }
 
