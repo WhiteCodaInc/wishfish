02a7b30a0a43c3be306dd4df2107de471713a7dd
WhiteCodaInc <notifications@whitecoda.com>
2015-05-25 12:51:49 +0530

    update

--- application/views/dashboard/top.php
+++ application/views/dashboard/top.php
@@ -448,7 +448,7 @@ $img_src = ($profile_pic != "") ?
                                     var id = $(this).prop('id');
                                     setTimeout(function () {
                                         $('.modal-backdrop').css('z-index', '999');
-                                    }, 1000);
+                                    }, 500);
                                     switch (id) {
                                         case "upload":
                                             $('#uploadSetup .modal-title').text("Complete Your Profile");
