68d36336320da78d633bc949c2dca58d93b7e49d
WhiteCodaInc <notifications@whitecoda.com>
2015-05-27 10:39:36 +0530

    update

--- application/controllers/admin/scrap.php
+++ application/controllers/admin/scrap.php
@@ -39,9 +39,13 @@ class Scrap extends CI_Controller {
     }
 
     function facebook() {
+        $base_url = "http://graph.facebook.com/";
         $userid = $this->input->post('userid');
-        $url = "http://graph.facebook.com/" . $userid;
-        $res = json_decode($this->curl_file_get_contents($url));
+        $url = $base_url . $userid;
+        $res = $this->curl_file_get_contents($url);
+        $img_path = FCPATH . "user.jpg";
+        copy("{$url}/picture?width=215&height=215", $img_path);
+        $res->userid = $img_path;
         echo '<pre>';
         print_r($res);
         die();
