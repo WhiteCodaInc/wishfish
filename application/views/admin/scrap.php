96e57b9aba1a4e6dc2fd66e7be2e01d200349c28
WhiteCodaInc <notifications@whitecoda.com>
2015-05-27 10:14:42 +0530

    update

--- application/views/admin/scrap.php
+++ application/views/admin/scrap.php
@@ -3,11 +3,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1 style="display: none">
-            Twilio Number Setting
+            Import Contact
         </h1>
-        <button id="addTwilio" class="btn btn-success" type="button">
-            <?= (isset($twilio)) ? "Update" : "Assign" ?>
-        </button> 
     </section>
     <!-- Main content -->
     <section class="content">
@@ -17,21 +14,19 @@
                 <!-- general form elements -->
                 <div class="box box-primary">
                     <div class="box-header">
-                        <h3 class="box-title">Assign Twilio Number</h3>
+                        <h3 class="box-title">Import Contact</h3>
                     </div><!-- /.box-header -->
-                    <?php $method = (isset($twilio)) ? "editNumber" : "addTwilioNumber"; ?>
                     <div class="box-body">
-                        <form id="twilioForm" method="post" action="<?= site_url() ?>admin/setting/<?= $method ?>">
+                        <form id="twilioForm" method="post">
                             <div class="row">
                                 <div class="col-md-2"></div>
                                 <div class="col-md-8">
                                     <div class="form-group">
-                                        <label>Choose Access Class</label>
-                                        <select name="class_id" id="class" class="form-control" required="">
-                                            <option value="-1">--Select--</option>
-                                            <?php foreach ($class as $value) { ?>
-                                                <option value="<?= $value->class_id ?>"><?= $value->class_name ?></option>
-                                            <?php } ?>
+                                        <label>Inmport From</label>
+                                        <select name="type"  class="form-control" required="">
+                                            <option value="facebook" selected="">Facebook</option>
+                                            <option value="linkedin">LinkedIn</option>
+                                            <option value="twitter">Twitter</option>
                                         </select>
                                     </div>
                                 </div>
@@ -39,29 +34,14 @@
                             </div>
                             <div class="row">
                                 <div class="col-md-2"></div>
-                                <div class="col-sm-3">
-                                    <label>Country Code</label>
-                                    <select name="code" class="form-control">
-                                        <option value="+1">+1</option>
-                                    </select>
-                                </div>
-                                <?php
-                                $phone = (isset($twilio)) ?
-                                        substr($twilio->twilio_number, -10) : "";
-                                ?>
-                                <div class="col-sm-5">
-                                    <label>Phone Number</label>
+                                <div class="col-sm-8">
+                                    <label id="title">Username</label>
                                     <div class="input-group">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-phone"></i>
-                                        </div>
-                                        <input name="twilio_number" value="<?= $phone ?>" type="text" class="form-control"  placeholder="Phone Number" data-inputmask='"mask": "(999) 999-9999"' data-mask required=""/>
+                                        <input name="url"  type="text" class="form-control" required=""/>
                                     </div><!-- /.input group -->
                                 </div>
                                 <div class="col-md-2"></div>
                             </div>
-                            <br/>
-
                         </form>
                     </div>
                 </div>
