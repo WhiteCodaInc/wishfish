6ca87cb13291bac951d6493e410596e8da5a2894
WhiteCodaInc <notifications@whitecoda.com>
2015-05-27 17:27:23 +0530

    update

--- application/controllers/app/calender.php
+++ application/controllers/app/calender.php
@@ -32,6 +32,7 @@ class Calender extends CI_Controller {
     }
 
     function index() {
+        die("User Called");
         $data['template'] = $this->objsmstemplate->getTemplates();
         $this->load->view('dashboard/header');
         $this->load->view('dashboard/top');
