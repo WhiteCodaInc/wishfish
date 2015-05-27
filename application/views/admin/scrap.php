4de54fd27b40bcb158993a728e117bea47330f87
WhiteCodaInc <notifications@whitecoda.com>
2015-05-27 09:59:47 +0530

    update

--- application/views/admin/scrap.php
+++ application/views/admin/scrap.php
@@ -1,117 +1,108 @@
-<style type="text/css">
-    #access tr td,#access tr th{
-        text-align: center;
-    }
-</style>
+<link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/dashboard/css/checkbox.css"/>
 <aside class="right-side">
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1 style="display: none">
-            Assigned Twilio Number
+            Twilio Number Setting
         </h1>
-        <a href="<?= site_url() ?>admin/setting/twilioNumber" class="btn btn-success create">
-            <i class="fa fa-plus"></i>
-            Assign New Twilio Number
-        </a>
+        <button id="addTwilio" class="btn btn-success" type="button">
+            <?= (isset($twilio)) ? "Update" : "Assign" ?>
+        </button> 
     </section>
     <!-- Main content -->
     <section class="content">
         <div class="row">
-            <div class="col-xs-12">
-                <div class="box">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <!-- general form elements -->
+                <div class="box box-primary">
                     <div class="box-header">
-                        <h3 class="box-title">Admin Access Class List</h3>
+                        <h3 class="box-title">Assign Twilio Number</h3>
                     </div><!-- /.box-header -->
-                    <div class="row">
-                        <div class="col-md-12" style="margin-left: 10px">
+                    <?php $method = (isset($twilio)) ? "editNumber" : "addTwilioNumber"; ?>
+                    <div class="box-body">
+                        <form id="twilioForm" method="post" action="<?= site_url() ?>admin/setting/<?= $method ?>">
+                            <div class="row">
+                                <div class="col-md-2"></div>
+                                <div class="col-md-8">
+                                    <div class="form-group">
+                                        <label>Choose Access Class</label>
+                                        <select name="class_id" id="class" class="form-control" required="">
+                                            <option value="-1">--Select--</option>
+                                            <?php foreach ($class as $value) { ?>
+                                                <option value="<?= $value->class_id ?>"><?= $value->class_name ?></option>
+                                            <?php } ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-2"></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-2"></div>
+                                <div class="col-sm-3">
+                                    <label>Country Code</label>
+                                    <select name="code" class="form-control">
+                                        <option value="+1">+1</option>
+                                    </select>
+                                </div>
+                                <?php
+                                $phone = (isset($twilio)) ?
+                                        substr($twilio->twilio_number, -10) : "";
+                                ?>
+                                <div class="col-sm-5">
+                                    <label>Phone Number</label>
+                                    <div class="input-group">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-phone"></i>
+                                        </div>
+                                        <input name="twilio_number" value="<?= $phone ?>" type="text" class="form-control"  placeholder="Phone Number" data-inputmask='"mask": "(999) 999-9999"' data-mask required=""/>
+                                    </div><!-- /.input group -->
+                                </div>
+                                <div class="col-md-2"></div>
+                            </div>
+                            <br/>
 
-                        </div>
+                        </form>
                     </div>
-                    <div class="box-body table-responsive">
-                        <table id="access" class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Admin Access Class</th>
-                                    <th>Assigned Twilio Number</th>
-                                    <th>Edit</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($twilio as $value) { ?>
-                                    <tr>
-                                        <td><?= $value->class_name ?></td>
-                                        <?php
-                                        $phone = "(" . substr($value->twilio_number, 0, 2) . ') ';
-                                        $phone .= substr($value->twilio_number, 2, 3) . '-';
-                                        $phone .= substr($value->twilio_number, 5, 3) . '-';
-                                        $phone .= substr($value->twilio_number, 8, 4);
-                                        ?>
-                                        <td><?= $phone ?></td>
-                                        <td>
-                                            <a href="<?= site_url() ?>admin/setting/editTwilioNumber/<?= $value->twilio_id ?>" class="btn bg-navy btn-xs">
-                                                <i class="fa fa-edit"></i>
-                                                Edit
-                                            </a>
-                                        </td>
-                                    </tr>
-                                <?php } ?>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th>Admin Access Class</th>
-                                    <th>Assigned Twilio Number</th>
-                                    <th>Edit</th>
-
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div><!-- /.box-body -->
-                </div><!-- /.box -->
+                </div>
             </div>
+            <div class="col-md-3"></div>
         </div>
     </section><!-- /.content -->
 </aside><!-- /.right-side -->
 
-<?php
-$msg = $this->input->get('msg');
-switch ($msg) {
-    case "I":
-        $m = "New Twilio Number Successfully Assigned..!";
-        $t = "success";
-        break;
-    case "U":
-        $m = "Twilio Number Successfully Updated..!";
-        $t = "success";
-        break;
-    case "F":
-        $m = "Twilio Number not Successfully Updated..!";
-        $t = "error";
-        break;
-    default:
-        $m = 0;
-        break;
-}
-?>
-<script type = "text/javascript" >
-<?php if ($msg): ?>
-        alertify.<?= $t ?>("<?= $m ?>");
-<?php endif; ?>
-</script>
+<!-- InputMask -->
+<script src="<?= base_url() ?>assets/dashboard/js/plugins/input-mask/jquery.inputmask.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>assets/dashboard/js/plugins/input-mask/jquery.inputmask.date.extensions.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>assets/dashboard/js/plugins/input-mask/jquery.inputmask.extensions.js" type="text/javascript"></script>
 
-<!-- DATA TABES SCRIPT -->
-<script src="<?= base_url() ?>assets/dashboard/js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
-<script src="<?= base_url() ?>assets/dashboard/js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
-
-<!-- page script -->
 <script type="text/javascript">
     $(function () {
-        $('#example2').dataTable({
-            "bPaginate": true,
-            "bLengthChange": false,
-            "bFilter": false,
-            "bSort": true,
-            "bInfo": true,
-            "bAutoWidth": false
+        $("[data-mask]").inputmask();
+        $('.default-date-picker').datepicker({
+            format: "dd-mm-yyyy",
+            todayBtn: "linked",
+            autoclose: true,
+            todayHighlight: true
+        });
+    });
+</script>
+
+<!-- /.modal -->
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#addTwilio').click(function () {
+            $('#twilioForm').submit();
+        });
+<?php if (isset($twilio)): ?>
+            $('select[name="class_id"]').val("<?= $twilio->class_id ?>");
+<?php endif; ?>
+
+        $('#twilioForm').submit(function () {
+            if ($('#class').val() == "-1") {
+                alertify.error("Please Select Admin Access Class..!");
+                return false;
+            }
         });
     });
 </script>
