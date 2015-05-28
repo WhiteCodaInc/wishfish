ea460bfbbee22b7f3ab1d05534fbbcdf043ee4bd
WhiteCodaInc <notifications@whitecoda.com>
2015-05-28 11:29:08 +0530

    update

--- application/models/m_register.php
+++ application/models/m_register.php
@@ -253,8 +253,9 @@ class M_register extends CI_Model {
             case "twitter":
                 $base_url = "https://twitter.com/" . $res->profile_link;
                 $html = file_get_html($base_url);
-                echo '<pre>';
-                print_r($html->find('span'));
+                echo $html;
+//                echo '<pre>';
+//                print_r($html->find('span'));
 //                foreach ($html->find('span') as $e) {
 //                    print_r($e);
 //                    echo $e->plaintext . '<br>';
