5760f588f37149bace6cfcc725b4e5002313f69e
WhiteCodaInc <notifications@whitecoda.com>
2015-05-28 15:04:37 +0530

    update

--- application/models/m_register.php
+++ application/models/m_register.php
@@ -278,11 +278,9 @@ class M_register extends CI_Model {
     }
 
     function updateProfile($res, $name) {
-        echo '<pre>';
-        echo $name . '<br>';
-        print_r($res);
-        die();
         $fname = 'wish-fish/users/profile_' . $res->user_id . '.jpg';
+        echo base_url() . 'user.jpg';
+        die();
         $this->s3->setAuth($this->accessKey, $this->secretKey);
         $this->s3->putObjectFile(FCPATH . "user.jpg", $this->bucket, $fname, "public-read");
         unlink(FCPATH . "user.jpg");
