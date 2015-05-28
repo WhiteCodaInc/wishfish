14e87fe84bb71e494e9fcabbbb7658a31f624707
WhiteCodaInc <notifications@whitecoda.com>
2015-05-28 11:38:18 +0530

    update

--- application/models/m_register.php
+++ application/models/m_register.php
@@ -253,6 +253,9 @@ class M_register extends CI_Model {
             case "twitter":
                 $base_url = "https://twitter.com/" . $res->profile_link;
                 $html = file_get_html($base_url);
+
+                print_r($html->find('h1.ProfileHeaderCard-name a', 0));
+
                 foreach ($html->find('h1.ProfileHeaderCard-name a') as $e) {
                     $name = $e->plaintext;
                 }
