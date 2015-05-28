ab907700e5c8d2c38c028f6cd68af383412e0ace
WhiteCodaInc <notifications@whitecoda.com>
2015-05-28 12:09:13 +0530

    update

--- application/libraries/authex.php
+++ application/libraries/authex.php
@@ -13,7 +13,7 @@ class Authex {
     }
 
     function logged_in() {
-        
+
         return ($this->_CI->session->userdata("profileid") ) ? true : false;
     }
 
@@ -32,7 +32,7 @@ class Authex {
 //    }
 
     function login($where) {
-        
+
         unset($where['remember']);
         $query = $this->_CI->db->get_where("admin_profile", $where);
         if ($query->num_rows() !== 1) {
@@ -49,7 +49,7 @@ class Authex {
             $query = $this->_CI->db->get_where('admin_profile', $where);
             $res = $query->row();
             $this->_CI->session->set_userdata('profileid', $res->profile_id);
-            $this->_CI->session->set_userdata('userid', $res->profile_id);
+//            $this->_CI->session->set_userdata('userid', $res->profile_id);
             $this->_CI->session->set_userdata('name', $res->fname . ' ' . $res->lname);
             $this->_CI->session->set_userdata('email', $res->email);
             $this->_CI->session->set_userdata('phone', $res->phone);
@@ -60,12 +60,12 @@ class Authex {
     }
 
     function logout() {
-        
+
         $this->_CI->session->sess_destroy();
     }
 
     function can_register($userid) {
-        
+
 
         $query = $this->_CI->db->get_where("admin_profile", array("userid" => $userid));
 
@@ -73,7 +73,7 @@ class Authex {
     }
 
     function isTrue($password) {
-        
+
         $this->_CI->db->select('password');
         $query = $this->_CI->db->get_where("admin_profile", array("userid" => $this->_CI->session->userdata("userid")));
 
