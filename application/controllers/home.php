9ab59396a04b6a25ed18e413965cadb60f4913bc
WhiteCodaInc <notifications@whitecoda.com>
2015-05-29 09:23:31 +0530

    update

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -23,10 +23,10 @@ class Home extends CI_Controller {
     }
 
     function index($term) {
-        
+
         echo $term;
         die();
-        
+
         $data['word'] = $this->wi_common->getRandomDigit(5);
         $this->session->set_userdata('captchaWord', $data['word']);
         $data['pdetail'] = $this->wi_common->getPlans();
