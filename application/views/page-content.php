bbc7fd759f4e524e053e1df6a9e580ec8cfb3dc6
WhiteCodaInc <notifications@whitecoda.com>
2015-05-29 14:19:51 +0530

    update

--- application/views/page-content.php
+++ application/views/page-content.php
@@ -5,10 +5,10 @@
             <div class="details-full-width">
                 <div class="col-sm-8 col-sm-offset-2">
                     <!--  Heading-->
-                    <div class="heading black-text">
+<!--                    <div class="heading black-text">
                         <div class="title"><h2><?= $page->title ?></h2></div>
                         <div class="subtitle"><h5>This is subtile. lorem ipsum dolar shit amet</h5></div>
-                    </div>
+                    </div>-->
                     <p class=" wow fadeInUp animated" data-wow-offset="10" data-wow-duration="1.5s">
                         <?= $page->content ?>
                     </p>
