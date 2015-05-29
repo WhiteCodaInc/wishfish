5fd3c3835f2a785e205214a5a77b7316f1fb2398
WhiteCodaInc <notifications@whitecoda.com>
2015-05-29 09:59:14 +0530

    update

--- application/views/page-content.php
+++ application/views/page-content.php
@@ -6,11 +6,11 @@
                 <div class="col-sm-8 col-sm-offset-2 text-center wow fadeIn animated" data-wow-offset="120" data-wow-duration="1.5s">
                     <!--  Heading-->
                     <div class="heading black-text">
-                        <div class="title"><h2>Build your Love with <b>wow</b> Landing Page Builder</h2></div>
+                        <div class="title"><h2><?= $page->title ?></h2></div>
                         <div class="subtitle"><h5>This is subtile. lorem ipsum dolar shit amet</h5></div>
                     </div>
                     <p class=" wow fadeInUp animated" data-wow-offset="10" data-wow-duration="1.5s">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets.
+                        <?= $page->content ?>
                     </p>
                 </div>
             </div>
