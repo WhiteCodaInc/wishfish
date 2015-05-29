3411468fbf170ae772a11bb29c1210386ec5d2c8
WhiteCodaInc <notifications@whitecoda.com>
2015-05-29 14:22:06 +0530

    update

--- application/views/page-content.php
+++ application/views/page-content.php
@@ -3,12 +3,12 @@
     <div class="container">
         <div class="row">
             <div class="details-full-width">
-                <div class="col-sm-8 col-sm-offset-2">
+                <div class="col-sm-8 col-sm-offset-2" style="text-align: justify">
                     <!--  Heading-->
-<!--                    <div class="heading black-text">
-                        <div class="title"><h2><?= $page->title ?></h2></div>
-                        <div class="subtitle"><h5>This is subtile. lorem ipsum dolar shit amet</h5></div>
-                    </div>-->
+                    <!--                    <div class="heading black-text">
+                                            <div class="title"><h2><?= $page->title ?></h2></div>
+                                            <div class="subtitle"><h5>This is subtile. lorem ipsum dolar shit amet</h5></div>
+                                        </div>-->
                     <p class=" wow fadeInUp animated" data-wow-offset="10" data-wow-duration="1.5s">
                         <?= $page->content ?>
                     </p>
