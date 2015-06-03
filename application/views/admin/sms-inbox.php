c0c8e100756e330f85f19d952132069f659aff30
WhiteCodaInc <notifications@whitecoda.com>
2015-06-03 10:58:20 +0530

    update

--- application/views/admin/sms-inbox.php
+++ application/views/admin/sms-inbox.php
@@ -170,7 +170,7 @@ switch ($msg) {
             // run the effect
             $(".effect").toggle("slide", options, 500);
         }
-        $('#inbox-data-table tbody tr').bind('live', function () {
+        $('#inbox-data-table tbody tr').live(function () {
             $msg = $(this).find('td.status > span').text();
             if ($msg == "Unread") {
                 $(this).find('td.status > span').removeClass('btn-danger');
