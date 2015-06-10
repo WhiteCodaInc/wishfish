7221a60d42a8b957d5333d8b1411fe0a7c5e7b56
WhiteCodaInc <notifications@whitecoda.com>
2015-06-10 09:19:13 +0530

    update

--- application/views/dashboard/dummy-dashboard.php
+++ application/views/dashboard/dummy-dashboard.php
@@ -222,29 +222,25 @@
         <aside class="right-side" >
             <div class="overlay" style="">
                 <div class="msg">
-                    <?php if (!$flag): ?>
+                    <div class="welcome" style="display: none">
                         <h1 style="text-align: center">Welcome to Wish-Fish!<br/>Please click the 'Activate Your Account' Button in your Email To Get Started!</h1>
                         <h3>Didn't get our Email? No worries, <a href="javascript:void(0);" id="active">Click Here</a> To Active Your Account.</h3>
-                    <?php else : ?>
-                        <div class="row" >
-                            <div class="col-md-3"></div>
-                            <form id="passForm" action="<?= site_url() ?>app/dashboard/updatePassword" method="post">
-                                <div class="col-md-6" style="text-align: center;">
-                                    <?php if (!$isForgot) : ?>
-                                        <lable><h2>Email confirmed!</h2></lable>
-                                    <?php endif; ?>
-                                    <lable><h2>Please set a password:</h2></lable>
-                                    <input id="passwd" type="password" name="password" placeholder="Please Enter a New Password" class="form-control" required /><br/>
-                                    <input id="confirm_passwd" type="password" placeholder="Please Confirm your new password" class="form-control" required /><br/>
-                                    <button  type="submit" class="btn btn-primary">Let's Get Started!</button> <br/>
-                                    <span id="msgPass" style="color: red"></span>
-                                </div>
-                                <input type="hidden" name="userid" value="<?= $userInfo->user_id ?>" />
-                                <input type="hidden" name="type" value="<?= ($isForgot) ? "forgot" : "welcome" ?>" />
-                            </form>
-                            <div class="col-md-3"></div>
-                        </div>
-                    <?php endif; ?>
+                    </div>
+                    <div class="row set-passwd" style="display: none" >
+                        <div class="col-md-3"></div>
+                        <form id="passForm" action="<?= site_url() ?>app/dashboard/updatePassword" method="post">
+                            <div class="col-md-6" style="text-align: center;">
+                                <lable><h2>Please set a password:</h2></lable>
+                                <input id="passwd" type="password" name="password" placeholder="Please Enter a New Password" class="form-control" required /><br/>
+                                <input id="confirm_passwd" type="password" placeholder="Please Confirm your new password" class="form-control" required /><br/>
+                                <button  type="submit" class="btn btn-primary">Let's Get Started!</button> <br/>
+                                <span id="msgPass" style="color: red"></span>
+                            </div>
+                            <input type="hidden" name="userid" value="<?= $userInfo->user_id ?>" />
+                            <input type="hidden" name="type" value="<?= ($isForgot) ? "forgot" : "welcome" ?>" />
+                        </form>
+                        <div class="col-md-3"></div>
+                    </div>
                 </div>
             </div>
             <section class="content-header">
@@ -269,10 +265,24 @@
             </div><!-- /.container -->
         </footer>
         <script type="text/javascript">
+            $(function () {
+<?php if (!$flag): ?>
+                    $('div.welcome').show();
+                    $('div.set-passwd').hide();
+<?php else: ?>
+                    $('div.welcome').hide();
+                    $('div.set-passwd').show();
+<?php endif; ?>
+            });
             $(document).ready(function () {
 
                 var pass = 1;
                 var confirmpass = 1;
+
+                $('#active').on('click', function () {
+                    $('div.welcome').hide();
+                    $('div.set-passwd').show();
+                });
 
                 $('#passwd').focusout(function () {
                     var passwd = $(this).val();
