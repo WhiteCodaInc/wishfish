008d3bdde5384f1aa6fe7e4e4869e241de1fa434
WhiteCodaInc <notifications@whitecoda.com>
2015-06-11 16:08:54 +0530

    update

--- application/libraries/authex.php
+++ application/libraries/authex.php
@@ -13,7 +13,6 @@ class Authex {
     }
 
     function logged_in() {
-
         return ($this->_CI->session->userdata("profileid") ) ? true : false;
     }
 
@@ -32,7 +31,6 @@ class Authex {
 //    }
 
     function login($where) {
-
         unset($where['remember']);
         $query = $this->_CI->db->get_where("admin_profile", $where);
         if ($query->num_rows() !== 1) {
@@ -59,23 +57,17 @@ class Authex {
     }
 
     function logout() {
-
         $this->_CI->session->sess_destroy();
     }
 
     function can_register($userid) {
-
-
         $query = $this->_CI->db->get_where("admin_profile", array("userid" => $userid));
-
         return ($query->num_rows() < 1) ? true : false;
     }
 
     function isTrue($password) {
-
         $this->_CI->db->select('password');
         $query = $this->_CI->db->get_where("admin_profile", array("userid" => $this->_CI->session->userdata("userid")));
-
         return ($password == $query->row()->password) ? true : false;
     }
 
