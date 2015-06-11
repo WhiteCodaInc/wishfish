b5ad858af3913f4a3d0ee5eae4d14e636f24c01f
WhiteCodaInc <notifications@whitecoda.com>
2015-06-11 17:12:16 +0530

    update

--- application/controllers/app/import.php
+++ application/controllers/app/import.php
@@ -110,6 +110,9 @@ class Import extends CI_Controller {
 
     function addContacts() {
         $post = $this->input->post();
+        echo '<pre>';
+        print_r($post);
+        die();
         foreach ($post['contact'] as $value) {
             $name = explode(' ', $post['name'][$value]);
             $set = array(
