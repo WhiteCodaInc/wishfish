a45a34eaff0f90f55c3c4a1b2c786144548b32d0
WhiteCodaInc <notifications@whitecoda.com>
2015-06-11 17:21:27 +0530

    update

--- application/controllers/app/import.php
+++ application/controllers/app/import.php
@@ -109,9 +109,6 @@ class Import extends CI_Controller {
 
     function addContacts() {
         $post = $this->input->post();
-        echo '<pre>';
-        print_r($post);
-        die();
         foreach ($post['contact'] as $value) {
             $name = explode(' ', $post['name'][$value]);
             $set = array(
@@ -121,7 +118,9 @@ class Import extends CI_Controller {
             );
             ($post['email'][$value]) ? $set['email'] = $post['email'][$value] : '';
             ($post['phone'][$value]) ? $set['phone'] = $post['phone'][$value] : '';
-
+            echo '<pre>';
+            print_r($set);
+            die();
             $this->db->insert('wi_contact_detail', $set);
         }
         header('location:' . site_url() . 'app/contacts');
