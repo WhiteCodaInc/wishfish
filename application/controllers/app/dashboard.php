1d6969110cf15f2c546d180c65430e4f5cd18773
WhiteCodaInc <notifications@whitecoda.com>
2015-06-12 17:02:14 +0530

    update

--- application/controllers/app/dashboard.php
+++ application/controllers/app/dashboard.php
@@ -240,9 +240,9 @@ class Dashboard extends CI_Controller {
             "events" => $ev,
             "legend" => $legend
         );
-        echo '<pre>';
-        print_r(json_encode($initialize));
-        die();
+//        echo '<pre>';
+//        print_r(json_encode($initialize));
+//        die();
         echo "[" . json_encode($initialize) . "]";
     }
 
