337f2eee2a3e2e10d620dbad9eac9d860df23158
WhiteCodaInc <notifications@whitecoda.com>
2015-06-19 14:08:55 +0530

    update

--- application/controllers/app/cal.php
+++ application/controllers/app/cal.php
@@ -53,7 +53,8 @@ class Cal extends CI_Controller {
                 } catch (Google_ServiceException $exc) {
                     echo '<pre>';
                     echo $exc->getMessage();
-                    print_r($exc);
+                    print_r($exc->getErrors());
+//                    print_r($exc);
 //                    print_r($exc->errors);
                 }
                 die();
