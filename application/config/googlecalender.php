a53900326db0800f47a6fa6e7d2ed99924943b1f
WhiteCodaInc <notifications@whitecoda.com>
2015-06-19 12:00:25 +0530

    update

---
+++ application/config/googlecalender.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['googlecalender']['application_name'] = 'Wish-Fish';
+$config['googlecalender']['client_id'] = '531642364953-tcuss6fd1nklabm38n3ucim4d8ka4sc2.apps.googleusercontent.com';
+$config['googlecalender']['client_secret'] = 'vbWqRKud5jjwf5kE7Et7Q9OY';
+$config['googlecalender']['redirect_uri'] = 'https://www.wish-fish.com/app/cal/events';
+$config['googlecalender']['api_key'] = 'AIzaSyDlhC9ZCWLDiIx6p7dqaYzcDpuHDOsJ03w';
+
---
+++ application/controllers/app/cal.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of dashboard
+ *
+ * @author Laxmisoft
+ */
+class Cal extends CI_Controller {
+
+    var $sess_data = array(), $userid;
+
+    //put your code here
+    function __construct() {
+        parent::__construct();
+        require APPPATH . 'third_party/google-api/Google_Client.php';
+        $this->config->load('googlecalender');
+
+        $this->client = new Google_Client();
+        $this->client->setApplicationName($this->config->item('application_name', 'googlecalender'));
+        $this->client->setClientId($this->config->item('client_id', 'googlecalender'));
+        $this->client->setClientSecret($this->config->item('client_secret', 'googlecalender'));
+        $this->client->setRedirectUri($this->config->item('redirect_uri', 'googlecalender'));
+        $this->client->setDeveloperKey($this->config->item('api_key', 'googlecalender'));
+        $this->client->setScopes("https://www.googleapis.com/auth/calendar.readonly");
+
+        $this->service = new Google_Service_Calendar($this->client);
+    }
+
+    function index() {
+        $this->client->setApprovalPrompt('auto');
+        header('location:' . $this->client->createAuthUrl());
+    }
+
+    public function events() {
+        if ($this->input->get('error') == "access_denied") {
+            header('location:' . site_url() . 'app/dashboard');
+        }
+        $code = $this->input->get('code');
+
+        if (isset($code) && $code != "") {
+            $this->client->authenticate($code);
+            $this->session->set_userdata('token', $this->client->getAccessToken());
+            if ($this->client->getAccessToken()) {
+                $calendarList = $this->service->calendarList->listCalendarList();
+
+                while (true) {
+                    foreach ($calendarList->getItems() as $calendarListEntry) {
+
+                        echo $calendarListEntry->getSummary() . "<br>\n";
+
+
+                        // get events 
+                        $events = $this->service->events->listEvents($calendarListEntry->id);
+
+
+                        foreach ($events->getItems() as $event) {
+                            echo "-----" . $event->getSummary() . "<br>";
+                        }
+                    }
+                    $pageToken = $calendarList->getNextPageToken();
+                    if ($pageToken) {
+                        $optParams = array('pageToken' => $pageToken);
+                        $calendarList = $this->service->calendarList->listCalendarList($optParams);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+}
