074cb25d4eaf78833fefa751735e335352ca1cb5
WhiteCodaInc <notifications@whitecoda.com>
2015-06-19 09:23:53 +0530

    update

--- application/views/dashboard/calender.php
+++ application/views/dashboard/calender.php
@@ -1031,7 +1031,7 @@ $userInfo = $this->wi_common->getUserInfo($this->session->userdata('userid'));
             autoclose: true,
             todayHighlight: true
         }).on('changeDate', function (ev) {
-            $('#neweventForm input[name="birthday"]').focusout();
+            $('#neweventForm input[name="date"]').focusout();
         });
 
         $('.set_repeat').click(function () {
@@ -1278,7 +1278,7 @@ $userInfo = $this->wi_common->getUserInfo($this->session->userdata('userid'));
             });
         });
 
-        $('#neweventForm input[name="birthday"]').focusout(function () {
+        $('#neweventForm input[name="date"]').focusout(function () {
             var dt = $(this).val();
             console.log("DATE : " + dt);
         });
