5dbadd6c31da247ebff352d9af1e7106c291b073
WhiteCodaInc <notifications@whitecoda.com>
2015-06-20 15:20:39 +0530

    update

--- application/controllers/app/calender.php
+++ application/controllers/app/calender.php
@@ -46,7 +46,7 @@ class Calender extends CI_Controller {
             $this->client->authenticate($this->input->get('code'));
             $token = json_decode($this->client->getAccessToken());
             $this->session->set_userdata('token', $token->access_token);
-            redirect('app/calender');
+            header('location' . site_url() . 'app/calender');
         }
         $data['template'] = $this->objsmstemplate->getTemplates();
         $this->load->view('dashboard/header');
