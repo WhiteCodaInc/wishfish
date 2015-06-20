6cdb5ea4344e9014d2d5de01314504ebff855162
WhiteCodaInc <notifications@whitecoda.com>
2015-06-20 17:46:41 +0530

    update

--- application/views/dashboard/user-setting.php
+++ application/views/dashboard/user-setting.php
@@ -41,7 +41,20 @@
 
                             <div class="form-group">
                                 <ul>
-                                    <li>1. https://console.developers.google.com/project/</li>
+                                    <li>1. First Open  <a href="https://console.developers.google.com/project/">
+                                            https://console.developers.google.com/project/ 
+                                        </a> and login with google account.
+                                    </li>
+                                    <li>2. Create a new project.</li>
+                                    <li>3. Enter your project name.</li>
+                                    <li>4. Click on APIs & auth from left an sidebar and click on Credentials</li>
+                                    <li>4. Click on 'create Client ID' and choose 'web application' click on configure consent screen.</li>
+                                    <li>5. On Consent screen Enter Project name as a 'Wish-Fish' and save it.</li>
+                                    <li>6. Enter Authorized redirect URIs "https://www.wish-fish.com/app/calender".</li>
+                                    <li>7. Copy Client ID and Client secret key and paste it on above setting form.</li>
+                                    <li>8. Click on 'Create new key' and select 'Server key'.</li>
+                                    <li>9. Enter following IP Address '50.28.18.90'.</li>
+                                    <li>9. Copy API key and paste it on above setting form.</li>
                                 </ul>
                             </div>
                         </div>
