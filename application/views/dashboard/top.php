72ee3d1c92602a0467c3740d616636a671c78d65
WhiteCodaInc <notifications@whitecoda.com>
2015-06-20 11:04:38 +0530

    update

--- application/views/dashboard/top.php
+++ application/views/dashboard/top.php
@@ -473,7 +473,7 @@ $userid = $this->session->userdata('userid');
                                 <div class="pull-left" id="wishfish-profile">
                                     <a href="<?= site_url() ?>app/profile" class="btn btn-default btn-flat">Profile</a>
                                 </div>
-                                <div class="pull-left">
+                                <div class="pull-left" style="margin-left: 1px;">
                                     <a href="<?= site_url() ?>app/setting" class="btn btn-default btn-flat">Setting</a>
                                 </div>
                                 <div class="pull-right">
