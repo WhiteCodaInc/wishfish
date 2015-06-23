eec942c65d854094556ae044e14965a2123a43db
WhiteCodaInc <notifications@whitecoda.com>
2015-06-23 16:27:36 +0530

    update

--- application/controllers/app/calender.php
+++ application/controllers/app/calender.php
@@ -343,8 +343,8 @@ class Calender extends CI_Controller {
                                     $freq = "YEARLY";
                                     break;
                             }
-//                            $recur = "RRULE:FREQ={$freq};INTERVAL={$post['freq_no']}";
-                            $recur = 'RRULE:FREQ=DAILY;COUNT=2';
+                            $recur = "RRULE:FREQ={$freq};INTERVAL={$post['freq_no']}";
+//                            $recur = 'RRULE:FREQ=DAILY;COUNT=2';
                         } else {
                             $recur = NULL;
                         }
