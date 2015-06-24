5b65f678c2b7bb8562bc70de8a38b763557cd685
WhiteCodaInc <notifications@whitecoda.com>
2015-06-24 13:52:54 +0530

    update

--- application/controllers/app/calender.php
+++ application/controllers/app/calender.php
@@ -471,9 +471,10 @@ class Calender extends CI_Controller {
     }
 
     function updateGoogleEvent($post) {
+        echo 'CALLED';
         $eventInfo = $this->objcal->getEventInfo($post['eventid']);
         print_r($eventInfo);
-        die();
+        die();  
         if ($eventInfo->google_event_id != "") {
 
             $calId = $this->getCalenderId();
