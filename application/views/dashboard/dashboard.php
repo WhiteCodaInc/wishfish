9a17b793d026c9e55e779493ebbfd04c1bbeb387
WhiteCodaInc <notifications@whitecoda.com>
2015-07-01 14:07:15 +0530

    update

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -9,7 +9,7 @@
     #p1 {
         margin:32px;
         margin-bottom:0;
-        height:450px;
+        height:600px;
     }
     #p1 #tg-container{
         z-index: 0
@@ -59,14 +59,14 @@
             <i class="fa fa-plus"></i>
             Create New Event
         </a>
-        <a href="<?= site_url() ?>app/sms_template/addTemplate" class="btn btn-warning btn-sm create">
-            <i class="fa fa-plus"></i>
-            Create SMS Template
-        </a>
-        <a href="<?= site_url() ?>app/email_template/addTemplate" class="btn btn-danger btn-sm create">
-            <i class="fa fa-plus"></i>
-            Create Email Template
-        </a>
+<!--        <a href="<?= site_url() ?>app/sms_template/addTemplate" class="btn btn-warning btn-sm create">
+        <i class="fa fa-plus"></i>
+        Create SMS Template
+    </a>
+    <a href="<?= site_url() ?>app/email_template/addTemplate" class="btn btn-danger btn-sm create">
+        <i class="fa fa-plus"></i>
+        Create Email Template
+    </a>-->
         <div class="box box-solid box-primary collapsed-box event">
             <div class="box-header" data-widget="collapse"  style="cursor: pointer;padding: 7px">
                 <h4 class="box-title" style="font-size: 15px">Events</h4>
@@ -168,185 +168,76 @@
 
     <!-- Main content -->
     <section class="content">
-        <div class="row m-bot15">
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-red">
-                    <div class="inner">
-                        <h3>
-                            <span><?= count($totalD) ?></span>
-                        </h3>
-                        <p>
-                            Today
-                        </p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-pie-graph"></i>
-                    </div>
-                </div>
-            </div><!-- ./col -->
-
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-yellow">
-                    <div class="inner">
-                        <h3>
-                            <span><?= count($totalW) ?></span>
-                        </h3>
-                        <p>
-                            This Week
-                        </p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-
-                </div>
-            </div><!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-green">
-                    <div class="inner">
-                        <h3>
-                            <span><?= count($totalM) ?></span>
-                        </h3>
-                        <p>
-                            This Month
-                        </p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
-                    </div>
-                </div>
-            </div><!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-aqua">
-                    <div class="inner">
-                        <h3>
-                            <span id="totalY">
-                                <?= count($totalY) ?>
-                            </span>
-                        </h3>
-                        <p>
-                            This Year
-                        </p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-
-                </div>
-            </div><!-- ./col -->
-        </div><!-- /.row -->
-        <!--        <div class="row ui-sortable">
-                    <div class="col-md-8">
-        
-                    </div>
-                    <div class="col-md-4">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="box box-solid box-primary collapsed-box">
-                                    <div class="box-header" data-widget="collapse"  style="cursor: pointer">
-                                        <h3 class="box-title">Events</h3>
-                                    </div>
-                                    <div class="box-body"  style="display: none">
-                                        <div class="form-group-lg">
-                                            <div class="box box-primary">
-                                                <div class="box-header ui-sortable-handle" style="cursor: move;">
-                                                    <i class="ion ion-clipboard"></i>
-                                                    <h3 class="box-title">TODAY</h3>
-                                                </div> /.box-header 
-                                                <div class="box-body">
-                                                    <ul class="todo-list ui-sortable">
-        <?php foreach ($totalD as $value) { ?>
-                                                                                                                                                        <li>
-                                                                                                                                                            <a href="<?= site_url() ?>app/calender?date=<?= $value->date ?>"
-                                                                                                                                                                drag handle 
-                                                                                                                                                               <span class="handle ui-sortable-handle">
-                                                                                                                                                                    <i class="fa fa-ellipsis-v"></i>
-                                                                                                                                                                    <i class="fa fa-ellipsis-v"></i>
-                                                                                                                                                                </span>
-                                                                                                                                                                 todo text 
-                                                                                                                                                                <span class="text"><?= $value->event ?></span>
-                                                                                                                                                                 Emphasis label 
-                                                                                                                                                                <small class="label" style="background-color: <?= $value->color ?>">
-                                                                                                                                                                    <i class="fa fa-clock-o"></i>
-            <?= $value->format_date ?>
-                                                                                                                                                                </small>
-                                                                                                                                                            </a>
-                                                                                                                                                        </li>
-        <?php } ?>
-                                                    </ul>
-                                                </div> /.box-body 
-                                            </div>
-                                        </div>
-                                        <div class="form-group-lg">
-                                            <div class="box box-primary">
-                                                <div class="box-header ui-sortable-handle" style="cursor: move;">
-                                                    <i class="ion ion-clipboard"></i>
-                                                    <h3 class="box-title">THIS WEEK</h3>
-                                                </div> /.box-header 
-                                                <div class="box-body">
-                                                    <ul class="todo-list ui-sortable">
-        <?php foreach ($totalW as $value) { ?>
-                                                                                                                                                        <li>
-                                                                                                                                                            <a href="<?= site_url() ?>app/calender?date=<?= $value->date ?>"
-                                                                                                                                                                drag handle 
-                                                                                                                                                               <span class="handle ui-sortable-handle">
-                                                                                                                                                                    <i class="fa fa-ellipsis-v"></i>
-                                                                                                                                                                    <i class="fa fa-ellipsis-v"></i>
-                                                                                                                                                                </span>
-                                                                                                                                                                 todo text 
-                                                                                                                                                                <span class="text"><?= $value->event ?></span>
-                                                                                                                                                                 Emphasis label 
-                                                                                                                                                                <small class="label" style="background-color: <?= $value->color ?>">
-                                                                                                                                                                    <i class="fa fa-clock-o"></i>
-            <?= $value->format_date ?>
-                                                                                                                                                                </small>
-                                                                                                                                                            </a>
-                                                                                                                                                        </li>
-        <?php } ?>
-                                                    </ul>
-                                                </div> /.box-body 
-                                            </div>
-                                        </div>
-                                        <div class="form-group-lg">
-                                            <div class="box box-primary">
-                                                <div class="box-header ui-sortable-handle" style="cursor: move;">
-                                                    <i class="ion ion-clipboard"></i>
-                                                    <h3 class="box-title">THIS MONTH</h3>
-                                                </div> /.box-header 
-                                                <div class="box-body">
-                                                    <ul class="todo-list ui-sortable">
-        <?php foreach ($totalM as $value) { ?>
-                                                                                                                                                        <li>
-                                                                                                                                                            <a href="<?= site_url() ?>app/calender?date=<?= $value->date ?>"
-                                                                                                                                                                drag handle 
-                                                                                                                                                               <span class="handle ui-sortable-handle">
-                                                                                                                                                                    <i class="fa fa-ellipsis-v"></i>
-                                                                                                                                                                    <i class="fa fa-ellipsis-v"></i>
-                                                                                                                                                                </span>
-                                                                                                                                                                 todo text 
-                                                                                                                                                                <span class="text"><?= $value->event ?></span>
-                                                                                                                                                                 Emphasis label 
-                                                                                                                                                                <small class="label" style="background-color: <?= $value->color ?>">
-                                                                                                                                                                    <i class="fa fa-clock-o"></i>
-            <?= $value->format_date ?>
-                                                                                                                                                                </small>
-                                                                                                                                                            </a>
-                                                                                                                                                        </li>
-        <?php } ?>
-                                                    </ul>
-                                                </div> /.box-body 
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+        <!--    
+            <div class="row m-bot15">
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-red">
+                        <div class="inner">
+                            <h3>
+                                <span><?= count($totalD) ?></span>
+                            </h3>
+                            <p>
+                                Today
+                            </p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
                         </div>
                     </div>
-                </div>-->
+                </div>
+    
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-yellow">
+                        <div class="inner">
+                            <h3>
+                                <span><?= count($totalW) ?></span>
+                            </h3>
+                            <p>
+                                This Week
+                            </p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+    
+                    </div>
+                </div>
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3>
+                                <span><?= count($totalM) ?></span>
+                            </h3>
+                            <p>
+                                This Month
+                            </p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3>
+                                <span id="totalY">
+        <?= count($totalY) ?>
+                                </span>
+                            </h3>
+                            <p>
+                                This Year
+                            </p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+    
+                    </div>
+                </div>
+            </div>
+        -->
+
         <style type="text/css">
             #p1{
                 margin: 0 !important;
