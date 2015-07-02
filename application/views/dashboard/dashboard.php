810dffc4cb9936e6964e8e213668d93894ecd49a
WhiteCodaInc <notifications@whitecoda.com>
2015-07-02 16:05:51 +0530

    update

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -194,6 +194,7 @@
 <script src="<?= base_url() ?>assets/dashboard/timeline/timeglider/TG_TimelineView.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?= base_url() ?>assets/dashboard/timeline/timeglider/TG_Mediator.js" type="text/javascript" charset="utf-8"></script> 
 <script src="<?= base_url() ?>assets/dashboard/timeline/timeglider/timeglider.timeline.widget.js" type="text/javascript"></script>
+
 <script src="<?= base_url() ?>assets/dashboard/timeline/timeglider/timeglider.datepicker.js" type="text/javascript"></script>
 
 
