b4e4b5ad5970202c4fb4cab80809c59e21b353cf
WhiteCodaInc <notifications@whitecoda.com>
2015-07-02 11:01:17 +0530

    update

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -70,7 +70,7 @@ class Login extends CI_Controller {
         }
         if (is_array($post) && count($post) > 0) {
             $is_login = $this->wi_authex->login($post);
-            if ($is_login == -1) {
+            if ($is_login == "-1") {
                 echo 'DEACTIVE';
 //                header('location:' . site_url() . 'login?msg=DA');
             } else if ($is_login) {
