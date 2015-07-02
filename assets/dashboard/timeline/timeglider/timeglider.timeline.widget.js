966f33d845d09b76be49ace4215f4891f4a6f86a
WhiteCodaInc <notifications@whitecoda.com>
2015-07-02 16:17:56 +0530

    update

--- assets/dashboard/timeline/timeglider/timeglider.timeline.widget.js
+++ assets/dashboard/timeline/timeglider/timeglider.timeline.widget.js
@@ -102,7 +102,14 @@
                     + "</div>"
                     + "</div>"
 
-
+//				+ "<div class='timeglider-slider-container noselect'>"
+//				+ "	<div class='tg-slider-plusminus tg-slider-plus tg-zoom-in'></div>"
+//				+ "	<div class='timeglider-slider'></div>"
+//				+ "	<div class='tg-slider-plusminus tg-slider-minus tg-zoom-out'></div>"
+//				+ "	<div class='timeglider-pan-buttons'>"
+//				+ "	<div class='timeglider-pan-left'></div><div class='timeglider-pan-right'></div>"
+//				+ "</div>"
+//				+ "</div>"
 
                     + "<div class='tg-scrim'></div>"
 
@@ -134,10 +141,10 @@
                     + "	<div class='timeglider-pan-left'></div><div class='timeglider-pan-right'></div>"
                     + "</div>"
                     + "</div>";
-            alert();
-            $('.tg-single-timeline-header').after(slider);
-            this.element.html(MAIN_TEMPLATE);
 
+            console.log($('.tg-widget-header'));
+
+            this.element.html(MAIN_TEMPLATE);
 
         }, // eof _create()
 
