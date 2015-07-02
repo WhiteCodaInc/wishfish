b869f779ff7d535728bc8ede043dee13a772afbb
WhiteCodaInc <notifications@whitecoda.com>
2015-07-02 16:30:12 +0530

    update

--- assets/dashboard/timeline/timeglider/timeglider.timeline.widget.js
+++ assets/dashboard/timeline/timeglider/timeglider.timeline.widget.js
@@ -146,16 +146,7 @@
          */
         _init: function () {
 
-            var slider = "<div class='timeglider-slider-container noselect'>"
-                    + "	<div class='tg-slider-plusminus tg-slider-plus tg-zoom-in'></div>"
-                    + "	<div class='timeglider-slider'></div>"
-                    + "	<div class='tg-slider-plusminus tg-slider-minus tg-zoom-out'></div>"
-                    + "	<div class='timeglider-pan-buttons'>"
-                    + "	<div class='timeglider-pan-left'></div><div class='timeglider-pan-right'></div>"
-                    + "</div>"
-                    + "</div>";
 
-            $('.tg-single-timeline-header').find('ul').after(slider);
 
             // validateOptions should come out as empty string
             var optionsCheck = timeglider.validateOptions(this.options);
