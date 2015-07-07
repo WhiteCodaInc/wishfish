369fef3102232b3c4519ced8f90a2378988bd881
WhiteCodaInc <notifications@whitecoda.com>
2015-07-07 10:21:00 +0530

    update

--- application/controllers/app/scrape.php
+++ application/controllers/app/scrape.php
@@ -41,7 +41,7 @@ class Scrape extends CI_Controller {
     }
 
     function fb() {
-        $base_url = "http://www.facebook.com/vishaltesting7";
+        $base_url = "https://www.facebook.com/vishaltesting7";
         $html = $this->curl_file_get_contents($base_url);
         echo $html;
     }
@@ -98,12 +98,12 @@ class Scrape extends CI_Controller {
     function curl_file_get_contents($url) {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url); //The URL to fetch. This can also be set when initializing a session with curl_init().
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, FALSE); //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE); //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5); //The number of seconds to wait while trying to connect.	
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE); //To follow any "Location: " header that the server sends as part of the HTTP header.
         curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE); //To automatically set the Referer: field in requests where it follows a Location: redirect.
         curl_setopt($curl, CURLOPT_TIMEOUT, 10); //The maximum number of seconds to allow cURL functions to execute.
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE); //To stop cURL from verifying the peer's certificate.
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); //To stop cURL from verifying the peer's certificate.
         $contents = curl_exec($curl);
         curl_close($curl);
         return $contents;
