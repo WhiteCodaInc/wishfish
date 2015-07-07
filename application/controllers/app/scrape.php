c8a01b9351c76d3471466b1d2a9308d3f3b65ba8
WhiteCodaInc <notifications@whitecoda.com>
2015-07-07 13:35:01 +0530

    update

--- application/controllers/app/scrape.php
+++ application/controllers/app/scrape.php
@@ -47,15 +47,17 @@ class Scrape extends CI_Controller {
         echo $html;
         $dom = new DOMDocument();
         @$dom->loadHTML($html, 0);
-        $title = $dom->getElementsByTagName('title');
-        print_r($title);
-//
-        echo '<pre>';
-        foreach ($title as $text) {
-            print_r($text) . '<br>';
-            $t = $title->getAttribute();
-            echo $t . '<br>';
-        }
+        $nodes = $dom->getElementsByTagName('title');
+        print_r($nodes);
+
+        $title = $nodes->item(0)->nodeValue;
+        echo $title;
+        
+//        foreach ($title as $text) {
+//            print_r($text) . '<br>';
+//            $t = $title->getAttribute();
+//            echo $t . '<br>';
+//        }
     }
 
 //    function facebook() {
@@ -110,7 +112,7 @@ class Scrape extends CI_Controller {
     function curl_file_get_contents($url) {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url); //The URL to fetch. This can also be set when initializing a session with curl_init().
-//        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE); //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5); //The number of seconds to wait while trying to connect.	
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE); //To follow any "Location: " header that the server sends as part of the HTTP header.
