c029d67c55f5e57cee9e0f71fe453e0fe0d01f3b
WhiteCodaInc <notifications@whitecoda.com>
2015-07-07 15:16:28 +0530

    update

--- application/views/dashboard/user-profile.php
+++ application/views/dashboard/user-profile.php
@@ -347,7 +347,7 @@
         $('#import').click(function () {
             var fid = $('#fbid').val();
             console.log(fid);
-            $('#profile-pic img.img-circle').prop('src', "https://graph.facebook.com/" + fid + "/picture?width=215&height=215")
+            $('#profile-pic img.img-circle').prop("src", "https://graph.facebook.com/" + fid + "/picture?width=215&height=215");
         });
 
 
