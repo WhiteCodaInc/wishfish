ed7aa3f59ac4df0cb2b93ed1c8f0e971b5ca28e7
WhiteCodaInc <notifications@whitecoda.com>
2015-07-08 11:41:07 +0530

    update

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -97,7 +97,7 @@ class Register extends CI_Controller {
                         header('Location: ' . site_url() . 'register?msg=RF');
                     }
                 } else {
-                    header('Location: ' . site_url() . 'login?$type=google&msg=R');
+                    header('Location: ' . site_url() . 'login?type=google&msg=R');
                 }
             } else {
                 header('Location: ' . site_url() . 'register?msg=RF');
@@ -148,7 +148,7 @@ class Register extends CI_Controller {
                         header('location: ' . site_url() . 'register?msg=RF');
                     }
                 } else {
-                    header('Location: ' . site_url() . 'login?$type=fb&msg=R');
+                    header('Location: ' . site_url() . 'login?type=fb&msg=R');
                 }
             } catch (FacebookApiException $e) {
                 error_log($e);
