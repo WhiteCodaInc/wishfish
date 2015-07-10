d7d3bba0e5b52f57a25ea942ce9db8e2d4bb0aba
WhiteCodaInc <notifications@whitecoda.com>
2015-07-10 13:24:45 +0530

    update

--- application/views/admin/pages.php
+++ application/views/admin/pages.php
@@ -125,6 +125,7 @@
             getPage(ids[pages.indexOf(page)]);
         });
         $('#pages').bind("focusout,keypress", function (e) {
+            alert(e.event);
             if (e.keyCode == 13) {
                 var page = $('#pages').val();
                 if (page.trim() != "") {
