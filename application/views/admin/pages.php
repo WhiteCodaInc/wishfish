8e0576ebb4904297fbfee1d386d8301d0104c5bb
WhiteCodaInc <notifications@whitecoda.com>
2015-07-10 13:28:41 +0530

    update

--- application/views/admin/pages.php
+++ application/views/admin/pages.php
@@ -125,7 +125,7 @@
             getPage(ids[pages.indexOf(page)]);
         });
         $('#pages').on("keypress", function (e) {
-            alert(e);
+            alert(e.type);
             if (e.keyCode == 13) {
                 var page = $('#pages').val();
                 if (page.trim() != "") {
