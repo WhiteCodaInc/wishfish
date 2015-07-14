848a9721d7fdd855e3ffd2f6bdf0a4e9f36a5d99
WhiteCodaInc <notifications@whitecoda.com>
2015-07-14 17:23:59 +0530

    update

--- application/libraries/wi_common.php
+++ application/libraries/wi_common.php
@@ -64,9 +64,6 @@ class Wi_common {
     function getCurrentPlan($userid = NULL) {
         $uid = ($userid == NULL) ? $this->user_id : $userid;
         $query = $this->_CI->db->get_where('wi_plan_detail', array('user_id' => $uid, 'plan_status' => 1));
-        echo '<pre>';
-        print_r($query->row());
-        die();
         return $query->row();
     }
 
