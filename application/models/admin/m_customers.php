c13fb5235192b018c014ae5f97683bab448172a0
WhiteCodaInc <notifications@whitecoda.com>
2015-07-16 14:26:34 +0530

    update

--- application/models/admin/m_customers.php
+++ application/models/admin/m_customers.php
@@ -25,6 +25,7 @@ class M_customers extends CI_Model {
         $this->accessKey = $this->encryption->decode($this->config->item('accessKey', 'aws'));
         $this->secretKey = $this->encryption->decode($this->config->item('secretKey', 'aws'));
 
+        $this->load->library('paypal_lib');
         $paypalGatewayInfo = $this->wi_common->getPaymentGatewayInfo("PAYPAL");
         $this->api_username = $paypalGatewayInfo->api_username;
         $this->api_password = $paypalGatewayInfo->api_password;
