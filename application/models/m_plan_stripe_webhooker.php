9648d7468204636679a4885d627492af0a64148b
WhiteCodaInc <notifications@whitecoda.com>
2015-07-22 10:19:24 +0530

    update

--- application/models/m_plan_stripe_webhooker.php
+++ application/models/m_plan_stripe_webhooker.php
@@ -26,8 +26,8 @@ class M_plan_stripe_webhooker extends CI_Model {
 
         $customer_id = $event_json->data->object->customer;
         $customer = Stripe_Customer::retrieve($customer_id);
-//        $myfile = fopen(FCPATH . 'events.txt', "a");
-//        fwrite($myfile, "Event :" . $event . "\n");
+        $myfile = fopen(FCPATH . 'events.txt', "a");
+        fwrite($myfile, "Event :" . $event . "\n");
 
 
 
