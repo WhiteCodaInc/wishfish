4025b56c48c445dea07439c85c737177eb9451eb
WhiteCodaInc <notifications@whitecoda.com>
2015-07-23 17:12:09 +0530

    update

--- application/views/home.php
+++ application/views/home.php
@@ -686,8 +686,8 @@
     <script
         src="https://checkout.stripe.com/checkout.js" class="stripe-button"
         data-key="<?= $stripe->publish_key ?>"
-        data-name="Personal"
-        data-description="Product"                    
+        data-name="$9.99"
+        data-description="1-month of Wish-Fish Personal"                    
         data-label="Stripe"                    
         >
     </script>
@@ -700,8 +700,8 @@
     <script
         src="https://checkout.stripe.com/checkout.js" class="stripe-button"
         data-key="<?= $stripe->publish_key ?>"
-        data-name="Enterprise"
-        data-description="Product"                    
+        data-name="$49.99"
+        data-description="1-month of Wish-Fish Enterprise"
         data-label="Stripe"                    
         >
     </script>
