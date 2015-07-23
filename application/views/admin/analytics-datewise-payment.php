e07ebc5ecced2e1a2fdc4b0f7cc180005ac60747
WhiteCodaInc <notifications@whitecoda.com>
2015-07-23 11:48:41 +0530

    update

--- application/views/admin/analytics-datewise-payment.php
+++ application/views/admin/analytics-datewise-payment.php
@@ -1,9 +1,17 @@
-<?php foreach ($phistory as $value) { ?>
-    <tr id="<?= $value->pdate ?>">
-        <td><?= date('m-d-Y', strtotime($value->pdate)) ?></td>
-        <td>
-            <a href="javascript:void(0);" class="totalP"><?= $value->totalP ?></a>
+<?php if (count($phistory) > 0): ?>
+    <?php foreach ($phistory as $value) { ?>
+        <tr id="<?= $value->pdate ?>">
+            <td><?= date('m-d-Y', strtotime($value->pdate)) ?></td>
+            <td>
+                <a href="javascript:void(0);" class="totalP"><?= $value->totalP ?></a>
+            </td>
+            <td>$ <?= number_format($value->totalA, 2) ?></td>
+        </tr>
+    <?php } ?>
+<?php else: ?>
+    <tr class="odd">
+        <td valign="top" colspan="2" class="dataTables_empty">
+            No data available in table
         </td>
-        <td>$ <?= number_format($value->totalA, 2) ?></td>
     </tr>
-<?php } ?>
+<?php endif; ?>
--- application/views/admin/analytics-datewise-user.php
+++ application/views/admin/analytics-datewise-user.php
@@ -1,15 +1,23 @@
-<?php foreach ($users as $value) { ?>
-    <tr id="<?= $value->date ?>">
-        <td><?= date('m-d-Y', strtotime($value->date)) ?></td>
-        <td>
-            <a href="javascript:void(0);" class="totalU"><?= $value->totalU ?></a>
+<?php if (count($users) > 0): ?>
+    <?php foreach ($users as $value) { ?>
+        <tr id="<?= $value->date ?>">
+            <td><?= date('m-d-Y', strtotime($value->date)) ?></td>
+            <td>
+                <a href="javascript:void(0);" class="totalU"><?= $value->totalU ?></a>
+            </td>
+            <td><?= $value->non_expired ?></td>
+            <td><?= $value->expired ?></td>
+            <td><?= $value->totalP ?></td>
+            <td><?= $value->totalE ?></td>
+            <td>$ <?= number_format($value->personal, 2) ?></td>
+            <td>$ <?= number_format($value->enterprise, 2) ?></td>
+            <td>$ <?= number_format($value->personal + $value->enterprise, 2) ?></td>
+        </tr>
+    <?php } ?>
+<?php else: ?>
+    <tr class="odd">
+        <td valign="top" colspan="2" class="dataTables_empty">
+            No data available in table
         </td>
-        <td><?= $value->non_expired ?></td>
-        <td><?= $value->expired ?></td>
-        <td><?= $value->totalP ?></td>
-        <td><?= $value->totalE ?></td>
-        <td>$ <?= number_format($value->personal, 2) ?></td>
-        <td>$ <?= number_format($value->enterprise, 2) ?></td>
-        <td>$ <?= number_format($value->personal + $value->enterprise, 2) ?></td>
     </tr>
-<?php } ?>
+<?php endif; ?>
