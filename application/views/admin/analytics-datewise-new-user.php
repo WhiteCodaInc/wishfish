f3c50ba78b3a487a69e8a22dfc93ef9fc48fcc5c
WhiteCodaInc <notifications@whitecoda.com>
2015-07-23 11:35:46 +0530

    update

--- application/views/admin/analytics-datewise-new-user.php
+++ application/views/admin/analytics-datewise-new-user.php
@@ -1,8 +1,18 @@
-<?php foreach ($users as $value) { ?>
-    <tr id="<?= $value->date ?>">
-        <td><?= date('m-d-Y', strtotime($value->date)) ?></td>
-        <td>
-            <a href="javascript:void(0);" class="totalU"><?= $value->totalU ?></a>
+<?php if (count($users) > 0): ?>
+    <?php foreach ($users as $value) { ?>
+        <tr id="<?= $value->date ?>">
+            <td><?= date('m-d-Y', strtotime($value->date)) ?></td>
+            <td>
+
+                <a href="javascript:void(0);" class="totalU"><?= $value->totalU ?></a>
+
+            </td>
+        </tr>
+    <?php } ?>
+<?php else: ?>
+    <tr class="odd">
+        <td valign="top" colspan="2" class="dataTables_empty">
+            No data available in table
         </td>
     </tr>
-<?php } ?>
+<?php endif; ?>
