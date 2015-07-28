e18576a96cd3a5301180f7c70c822bd7a37befaf
WhiteCodaInc <notifications@whitecoda.com>
2015-07-28 12:42:36 +0530

    update

--- application/controllers/admin/offers.php
+++ application/controllers/admin/offers.php
@@ -28,9 +28,9 @@ class Offers extends CI_Controller {
 
     function index() {
         $data['offers'] = $this->objoffer->getOffers();
-//        echo '<pre>';
-//        print_r($data);
-//        die();
+        echo '<pre>';
+        print_r($data);
+        die();
         $this->load->view('admin/admin_header');
         $this->load->view('admin/admin_top');
         $this->load->view('admin/admin_navbar');
