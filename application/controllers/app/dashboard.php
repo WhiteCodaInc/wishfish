c0f9c4473cd99f22f184d5a03fe4b205137c9a4e
WhiteCodaInc <notifications@whitecoda.com>
2015-07-30 17:53:48 +0530

    update

--- application/controllers/app/dashboard.php
+++ application/controllers/app/dashboard.php
@@ -66,6 +66,8 @@ class Dashboard extends CI_Controller {
                     $login = array(
                         'email' => $userInfo->email
                     );
+                    print_r($login);
+                    die();
                     if ($this->wi_authex->login($login)) {
                         header('location:' . site_url() . 'app/dashboard');
                     } else {
