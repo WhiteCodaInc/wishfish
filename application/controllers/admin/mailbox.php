a150bfa4bc5738bdcde05147e34995218028c3b9
WhiteCodaInc <notifications@whitecoda.com>
2015-07-31 16:27:53 +0530

    update

--- application/controllers/admin/mailbox.php
+++ application/controllers/admin/mailbox.php
@@ -133,7 +133,7 @@ class Mailbox extends CI_Controller {
             $threads = $this->makeThreads($mailbox);
             print_r($threads);
             die();
-            echo json_encode(array_reverse($threads[$subject]));
+//            echo json_encode(array_reverse($threads[$subject]));
         } else {
             header('location:' . site_url() . 'admin/mailbox/inbox');
         }
