46d546d35e82caf96820e7080c7ac86222084ccc
WhiteCodaInc <notifications@whitecoda.com>
2015-08-03 10:42:21 +0530

    update

--- application/controllers/admin/mailbox.php
+++ application/controllers/admin/mailbox.php
@@ -57,9 +57,9 @@ class Mailbox extends CI_Controller {
         $url = ($folder != "INBOX") ?
                 "{mail.mikhailkuznetsov.com:143/notls}INBOX.{$folder}" :
                 "{mail.mikhailkuznetsov.com:143/notls}INBOX";
-        echo $url . '<br>';
+//        echo $url . '<br>';
         $imap_obj = imap_check($this->stream);
-        echo '<pre>';
+//        echo '<pre>';
         print_r($imap_obj);
         if (!$imap_obj) {
             $mailbox = array();
@@ -70,9 +70,9 @@ class Mailbox extends CI_Controller {
         else {
             imap_reopen($this->stream, $url);
             $emails = imap_search($this->stream, 'ALL');
-            echo "<br>-------FOLDER : {$url}-----------<br>";
-            echo "<br>All Emails : <br>";
-            print_r($emails);
+//            echo "<br>-------FOLDER : {$url}-----------<br>";
+//            echo "<br>All Emails : <br>";
+//            print_r($emails);
             if (is_array($emails)) {
                 rsort($emails);
                 $data = array();
@@ -95,9 +95,9 @@ class Mailbox extends CI_Controller {
         }
         $data['folder'] = $this->getInboxFolder();
         $data['threads'] = $this->makeThreads($mailbox);
-        echo '<pre>';
-        print_r($data);
-        die();
+//        echo '<pre>';
+//        print_r($data);
+//        die();
         $this->load->view('admin/admin_header');
         $this->load->view('admin/admin_top');
         $this->load->view('admin/admin_navbar');
