9ccb94d22ea6842e3613c46ca89fd3b38d0e1701
WhiteCodaInc <notifications@whitecoda.com>
2015-08-10 17:00:50 +0530

    update

--- assets/hopscotch/demo.js
+++ assets/hopscotch/demo.js
@@ -113,7 +113,7 @@ var tour = {
         {
             target: 'wishfish-contact',
             placement: 'bottom',
-            yOffset: 'center',
+            xOffset: 'center',
             title: 'Great! Now Let`s Add Some Friends!',
             showPrevButton: false,
             onNext: function () {
