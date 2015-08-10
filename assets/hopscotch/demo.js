6b1b6faea14d1992b5c5e7c5117bd2752e5fad59
WhiteCodaInc <notifications@whitecoda.com>
2015-08-10 16:57:19 +0530

    update

--- assets/hopscotch/demo.js
+++ assets/hopscotch/demo.js
@@ -96,7 +96,7 @@ var tour = {
             target: 'select-timezone',
             showPrevButton: false,
             placement: 'left',
-            yOffset: 30,
+            yOffset: -60,
             title: 'Select Your Time Zone',
             arrowOffset: 30,
         },
