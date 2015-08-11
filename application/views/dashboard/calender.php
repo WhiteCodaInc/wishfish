b848b3c5467ac4782b2275d3a7dbbee69ec86736
WhiteCodaInc <notifications@whitecoda.com>
2015-08-11 15:01:28 +0530

    update

--- application/views/dashboard/calender.php
+++ application/views/dashboard/calender.php
@@ -1399,50 +1399,50 @@ $userInfo = $this->wi_common->getUserInfo($this->session->userdata('u_userid'));
             $('#' + form + ' input[name="notify"]').removeAttr('disabled');
             $('#' + form + ' input[name="notify"]:nth(0)').trigger('click');
 
-            if (form == "neweventForm") {
-
-                if (notify == "me") {
-                    var uphone = "<?= $userInfo->phone ?>";
-                    var uemail = "<?= $userInfo->email ?>";
-                    if (etype == "notification" || etype == "sms") {
-                        if (uphone == "") {
-                            $('#' + form + ' input[name="notify"]:nth(1)').prop('disabled', true);
-                            $notify = "This user does not have a valid phone number!";
-                            $('#' + form + ' input[name="notify"]:nth(1)').parent().prop('title', $notify);
-                        } else {
-                            $('#' + form + ' input[name="notify"]:nth(1)').parent().removeAttr('title');
-                        }
-                    } else {
-                        if (uemail == "") {
-                            $('#' + form + ' input[name="notify"]:nth(1)').prop('disabled', true);
-                            $notify = "This user does not have a valid email address!";
-                            $('#' + form + ' input[name="notify"]:nth(1)').parent().prop('title', $notify);
-                        } else {
-                            $('#' + form + ' input[name="notify"]:nth(1)').parent().removeAttr('title');
-                        }
-                    }
-                } else {
-                    var cphone = "<?= isset($contactInfo) ? $contactInfo->phone : "" ?>";
-                    var cemail = "<?= isset($contactInfo) ? $contactInfo->email : "" ?>";
-                    if (etype == "notification" || etype == "sms") {
-                        if (cphone == "") {
-                            $('#' + form + ' input[name="notify"]:nth(0)').prop('disabled', true);
-                            $notify = "This user does not have a valid phone number!";
-                            $('#' + form + ' input[name="notify"]:nth(0)').parent().prop('title', $notify);
-                        } else {
-                            $('#' + form + ' input[name="notify"]:nth(0)').parent().removeAttr('title');
-                        }
-                    } else {
-                        if (cemail == "") {
-                            $('#' + form + ' input[name="notify"]:nth(0)').prop('disabled', true);
-                            $notify = "This user does not have a valid email address!";
-                            $('#' + form + ' input[name="notify"]:nth(0)').parent().prop('title', $notify);
-                        } else {
-                            $('#' + form + ' input[name="notify"]:nth(0)').parent().removeAttr('title');
-                        }
-                    }
-                }
-            }
+//            if (form == "neweventForm") {
+//
+//                if (notify == "me") {
+//                    var uphone = "<?= $userInfo->phone ?>";
+//                    var uemail = "<?= $userInfo->email ?>";
+//                    if (etype == "notification" || etype == "sms") {
+//                        if (uphone == "") {
+//                            $('#' + form + ' input[name="notify"]:nth(1)').prop('disabled', true);
+//                            $notify = "This user does not have a valid phone number!";
+//                            $('#' + form + ' input[name="notify"]:nth(1)').parent().prop('title', $notify);
+//                        } else {
+//                            $('#' + form + ' input[name="notify"]:nth(1)').parent().removeAttr('title');
+//                        }
+//                    } else {
+//                        if (uemail == "") {
+//                            $('#' + form + ' input[name="notify"]:nth(1)').prop('disabled', true);
+//                            $notify = "This user does not have a valid email address!";
+//                            $('#' + form + ' input[name="notify"]:nth(1)').parent().prop('title', $notify);
+//                        } else {
+//                            $('#' + form + ' input[name="notify"]:nth(1)').parent().removeAttr('title');
+//                        }
+//                    }
+//                } else {
+//                    var cphone = "<?= isset($contactInfo) ? $contactInfo->phone : "" ?>";
+//                    var cemail = "<?= isset($contactInfo) ? $contactInfo->email : "" ?>";
+//                    if (etype == "notification" || etype == "sms") {
+//                        if (cphone == "") {
+//                            $('#' + form + ' input[name="notify"]:nth(0)').prop('disabled', true);
+//                            $notify = "This user does not have a valid phone number!";
+//                            $('#' + form + ' input[name="notify"]:nth(0)').parent().prop('title', $notify);
+//                        } else {
+//                            $('#' + form + ' input[name="notify"]:nth(0)').parent().removeAttr('title');
+//                        }
+//                    } else {
+//                        if (cemail == "") {
+//                            $('#' + form + ' input[name="notify"]:nth(0)').prop('disabled', true);
+//                            $notify = "This user does not have a valid email address!";
+//                            $('#' + form + ' input[name="notify"]:nth(0)').parent().prop('title', $notify);
+//                        } else {
+//                            $('#' + form + ' input[name="notify"]:nth(0)').parent().removeAttr('title');
+//                        }
+//                    }
+//                }
+//            }
 
             if (etype == "notification") {
                 $('#n_event_empty').show();
