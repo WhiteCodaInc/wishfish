437e72890b1581fbd5ae42f43a66beb6a390ed5a
WhiteCodaInc <notifications@whitecoda.com>
2015-08-11 15:39:16 +0530

    update

--- application/views/dashboard/calender.php
+++ application/views/dashboard/calender.php
@@ -973,13 +973,11 @@ $userInfo = $this->wi_common->getUserInfo($this->session->userdata('u_userid'));
         if ($(this).val() == "me") {
             if (event_type != "email" && "<?= $userInfo->phone ?>" == "") {
                 $(this).prop('checked', false);
-                (form == "eventForm") ?
-                        $('#' + form + ' input[name="notify"]:nth(0)').prop('checked', true) : "";
+               $('#' + form + ' input[name="notify"]:nth(0)').prop('checked', true);
                 alertify.alert("It's look like you have not enter phone number..!");
             } else if (event_type == "email" && "<?= $userInfo->email ?>" == "") {
                 $(this).prop('checked', false);
-                (form == "eventForm") ?
-                        $('#' + form + ' input[name="notify"]:nth(0)').prop('checked', true) : "";
+                $('#' + form + ' input[name="notify"]:nth(0)').prop('checked', true);
                 alertify.alert("It's look like you have not enter email address..!");
             } else {
                 if (form == "eventForm") {
