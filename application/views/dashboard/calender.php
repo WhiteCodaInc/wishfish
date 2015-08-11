9341e4440a61704e593659021bb807ee35729365
WhiteCodaInc <notifications@whitecoda.com>
2015-08-11 15:19:11 +0530

    update

--- application/views/dashboard/calender.php
+++ application/views/dashboard/calender.php
@@ -1002,11 +1002,11 @@ $userInfo = $this->wi_common->getUserInfo($this->session->userdata('u_userid'));
                 var cemail = "<?= isset($contactInfo) ? $contactInfo->email : "" ?>";
                 if (event_type != "email" && cphone == "") {
                     $(this).prop('checked', false);
-                    $('#' + form + ' input[name="notify"]:nth(1)').prop('checked', true);
+//                    $('#' + form + ' input[name="notify"]:nth(1)').prop('checked', true);
                     alertify.alert("It's look like you have not enter phone number..!");
                 } else if (event_type == "email" && cemail == "") {
                     $(this).prop('checked', false);
-                    $('#' + form + ' input[name="notify"]:nth(1)').prop('checked', true);
+//                    $('#' + form + ' input[name="notify"]:nth(1)').prop('checked', true);
                     alertify.alert("It's look like you have not enter email address..!");
                 } else {
                     var cid = "<?= isset($contactInfo) ? $contactInfo->contact_id : "" ?>";
@@ -1400,12 +1400,6 @@ $userInfo = $this->wi_common->getUserInfo($this->session->userdata('u_userid'));
                 $('#' + form + ' input[name="notify"]').removeAttr('disabled');
                 $('#' + form + ' input[name="notify"]:nth(0)').trigger('click');
             }
-
-
-
-
-
-
             if (etype == "notification") {
                 $('#n_event_empty').show();
                 $('#n_event_status').hide();
