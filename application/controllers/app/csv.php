78e89a2b86e65923550c4861bbfdc1401b1aac23
WhiteCodaInc <notifications@whitecoda.com>
2015-08-13 17:37:31 +0530

    update

--- application/controllers/app/csv.php
+++ application/controllers/app/csv.php
@@ -97,10 +97,6 @@ class Csv extends CI_Controller {
     function addContacts() {
         $post = $this->input->post();
         
-        echo '<pre>';
-        print_r($post);
-        die();
-        
         if (isset($post['contact']) && count($post['contact']) > 0) {
             foreach ($post['contact'] as $value) {
                 $set = array(
