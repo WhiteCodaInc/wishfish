a641a14736dd3629faafb47cc907f88f26b300da
WhiteCodaInc <notifications@whitecoda.com>
2015-08-13 09:51:45 +0530

    update

--- application/views/dashboard/csvindex.php
+++ application/views/dashboard/csvindex.php
@@ -1,5 +1,5 @@
 <h2>CI Addressbook Import</h2>
-<form method="post" action="<?= site_url() ?>csv/importcsv" enctype="multipart/form-data">
+<form method="post" action="<?= site_url() ?>app/csv/importcsv" enctype="multipart/form-data">
     <input type="file" name="userfile" ><br><br>
     <input type="submit" name="submit" value="UPLOAD" class="btn btn-primary">
 </form>
