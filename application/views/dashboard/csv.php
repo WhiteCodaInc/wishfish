da609296fa75c4ec11b42e1a0ea7b948a9c2d935
WhiteCodaInc <notifications@whitecoda.com>
2015-08-13 13:54:59 +0530

    update

--- application/views/dashboard/csv.php
+++ application/views/dashboard/csv.php
@@ -37,18 +37,18 @@
                         <div class="col-xs-12" style="margin-left: 1%">
                             <form id="csvForm" enctype="multipart/form-data" method="post">
                                 <div class="row">
-                                    <div class="col-md-3"></div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-2"></div>
+                                    <div class="col-md-3">
                                         <input name="upload"  type="file" class="form-control" />
                                     </div>
                                     <div class="col-md-2">
                                         <button class="btn btn-success" type="submit" id="csv">Upload</button>
                                     </div>
-                                    <div class="col-md-3"></div>
-                                </div>
-                                <br/>
-                                <div style="display: none;margin-top: 10px;" class="calert">
-                                    <span class="errorMsg"></span>
+                                    <div class="col-md-5">
+                                        <div style="display: none;margin-top: 10px;" class="calert">
+                                            <span class="errorMsg"></span>
+                                        </div>
+                                    </div>
                                 </div>
                             </form>
                         </div>
