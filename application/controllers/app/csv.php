fb351e9af1432c4164f468811a8f2c84ed8e5362
WhiteCodaInc <notifications@whitecoda.com>
2015-08-13 15:09:56 +0530

    update

--- application/controllers/app/csv.php
+++ application/controllers/app/csv.php
@@ -34,43 +34,48 @@ class Csv extends CI_Controller {
     }
 
     function importcsv() {
-        $error = "";
-        $config['upload_path'] = FCPATH . 'uploads/';
-        $config['allowed_types'] = '*';
-        $config['max_size'] = '1000';
 
-        $this->load->library('upload', $config);
+        if (!empty($_FILES)) {
+            $error = "";
+            $config['upload_path'] = FCPATH . 'uploads/';
+            $config['allowed_types'] = '*';
+            $config['max_size'] = '1000';
 
-        if (!$this->upload->do_upload('upload')) {
-            $error = $this->upload->display_errors();
-        } else {
-            $file_data = $this->upload->data();
-            $file_path = FCPATH . 'uploads/' . $file_data['file_name'];
+            $this->load->library('upload', $config);
 
-            $contacts = array();
-
-            if ($this->csvimport->get_array($file_path)) {
-                $csv_array = $this->csvimport->get_array($file_path);
-                foreach ($csv_array as $row) {
-                    $insert_data = array(
-                        'name' => $row['firstname'],
-                        'phone' => $row['phone'],
-                        'email' => $row['email']
-                    );
-                    $contacts[] = $insert_data;
-                }
-                $data['contacts'] = $contacts;
-                unlink($file_path);
+            if (!$this->upload->do_upload('upload')) {
+                $error = $this->upload->display_errors();
             } else {
-                $error = "Error occur during importing contact..! Try Again..!";
-                $data['contacts'] = $contacts;
+                $file_data = $this->upload->data();
+                $file_path = FCPATH . 'uploads/' . $file_data['file_name'];
+
+                $contacts = array();
+
+                if ($this->csvimport->get_array($file_path)) {
+                    $csv_array = $this->csvimport->get_array($file_path);
+                    foreach ($csv_array as $row) {
+                        $insert_data = array(
+                            'name' => $row['firstname'],
+                            'phone' => $row['phone'],
+                            'email' => $row['email']
+                        );
+                        $contacts[] = $insert_data;
+                    }
+                    $data['contacts'] = $contacts;
+                    unlink($file_path);
+                } else {
+                    $error = "Error occur during importing contact..! Try Again..!";
+                    $data['contacts'] = $contacts;
+                }
             }
+            $this->session->set_flashdata('error', $error);
+            $this->load->view('dashboard/header');
+            $this->load->view('dashboard/top');
+            $this->load->view('dashboard/csv', $data);
+            $this->load->view('dashboard/footer');
+        } else {
+            header('location:' . site_url() . 'app/csv');
         }
-        $this->session->set_flashdata('error', $error);
-        $this->load->view('dashboard/header');
-        $this->load->view('dashboard/top');
-        $this->load->view('dashboard/csv', $data);
-        $this->load->view('dashboard/footer');
     }
 
     function addContacts() {
