fd093e7479ebfce38d9c23c5c81edcd8af3fcd8f
WhiteCodaInc <notifications@whitecoda.com>
2015-08-17 11:27:20 +0530

    update

--- application/views/admin/media-preview.php
+++ application/views/admin/media-preview.php
@@ -8,7 +8,8 @@
         <div id='mediaplayer'></div>
         <script type="text/javascript">
             jwplayer('mediaplayer').setup({
-                file: 'rtmp://s12e6wqr7fb3zu.cloudfront.net/cfx/st/<?= $media->path ?>',
+//                file: 'rtmp://s12e6wqr7fb3zu.cloudfront.net/cfx/st/<?= $media->path ?>',
+                file: 'rtmp://s12e6wqr7fb3zu.cloudfront.net/cfx/st/video/video_14.mp4',
                 width: "auto",
                 height: "300"
             });
