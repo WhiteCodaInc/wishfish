964fe5270f8e8b62e306fda40af9344f47234fd1
WhiteCodaInc <notifications@whitecoda.com>
2015-08-18 15:38:12 +0530

    update

--- imagecrop/UploadImage.php
+++ imagecrop/UploadImage.php
@@ -351,13 +351,3 @@ class qqFileUploader {
     }
 
 }
-
-// list of valid extensions, ex. array("jpeg", "xml", "bmp")
-$allowedExtensions = array();
-// max file size in bytes
-$sizeLimit = 2 * 1024 * 1024;
-
-$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
-$result = $uploader->handleUpload('/home/wishfish/public_html/uploads/');
-// to pass data through iframe you will need to encode all html tags
-echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
