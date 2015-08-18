9dc826fbc6b0c89547ef216c922fae231c85c277
WhiteCodaInc <notifications@whitecoda.com>
2015-08-18 14:47:52 +0530

    update

--- assets/dashboard/ImageCrop/js/fileuploader.js
+++ assets/dashboard/ImageCrop/js/fileuploader.js
@@ -883,7 +883,7 @@ qq.UploadButton.prototype = {
 qq.UploadHandlerAbstract = function (o) {
     this._options = {
         debug: false,
-        action: '/home/wishfish/public_html/imagecrop/upload.php',
+        action: 'upload.php',
         // maximum number of concurrent uploads        
         maxConnections: 999,
         onProgress: function (id, fileName, loaded, total) {
