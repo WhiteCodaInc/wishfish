3390886a25efa97227edff04440fb07ddb60e1c5
WhiteCodaInc <notifications@whitecoda.com>
2015-08-19 09:08:04 +0530

    update

--- application/views/dashboard/user-profile.php
+++ application/views/dashboard/user-profile.php
@@ -67,7 +67,7 @@
                                         <a class="btn btn-success" data-backdrop="static" data-keyboard="false"  data-toggle="modal" data-target="#crop-modal">
                                             Upload Profile Pic
                                         </a>
-                                        <label>OR</label>
+                                        <label> OR </label>
                                         <a style="cursor: pointer;" class="fb"  data-toggle="modal" data-target="#import-modal">
                                             Import From Facebook
                                         </a>
