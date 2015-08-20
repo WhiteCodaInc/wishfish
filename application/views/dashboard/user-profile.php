8aedbfaf143eba4a633dccac1fcdaffe1db516e8
WhiteCodaInc <notifications@whitecoda.com>
2015-08-20 09:46:30 +0530

    update

--- application/views/dashboard/user-profile.php
+++ application/views/dashboard/user-profile.php
@@ -515,7 +515,7 @@
                 return false;
             }
             var email = $('#userForm input[name="email"]').val();
-            var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+            var filter = /^([\w-+\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
             if (email.trim() == "") {
                 alertify.error("Email is Required...!");
                 return false;
