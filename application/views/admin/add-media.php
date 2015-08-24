24595cd0814864bfa034255124b999eb7e3ffc7e
WhiteCodaInc <notifications@whitecoda.com>
2015-08-24 11:07:25 +0530

    update

--- application/views/admin/add-media.php
+++ application/views/admin/add-media.php
@@ -1,8 +1,28 @@
-<script type="text/javascript" src="https://malsup.github.com/jquery.form.js"></script>
+<script src="<?= base_url() ?>assets/dashboard/js/jquery.form.min.js" type="text/javascript"></script>
 <style>
-    .progress { position:relative; width:400px; border: 1px solid #ddd; padding: 1px; border-radius: 3px; }
-    .bar { background-color: #B4F5B4; width:0%; height:20px; border-radius: 3px; }
-    .percent { position:absolute; display:inline-block; top:3px; left:48%; }
+    #progressbox {
+        border: 1px solid #0099CC;
+        padding: 1px; 
+        position:relative;
+        width:400px;
+        border-radius: 3px;
+        margin: 10px;
+        display:none;
+        text-align:left;
+    }
+    #progressbar {
+        height:20px;
+        border-radius: 3px;
+        background-color: #003333;
+        width:1%;
+    }
+    #statustxt {
+        top:3px;
+        left:50%;
+        position:absolute;
+        display:inline-block;
+        color: #000000;
+    }
 </style>
 <aside class="right-side">
     <!-- Content Header (Page header) -->
@@ -51,7 +71,7 @@
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label>Upload</label>
-                                        <input name="upload"  type="file" class="form-control"  required=""/>
+                                        <input name="upload"  type="file" id="imageInput" class="form-control"  required=""/>
                                     </div>
                                 </div>
                             </div>
@@ -93,14 +113,14 @@
                                     </div>
                                 <?php } ?>
                             <?php endif; ?>
-                            <div class="row progress_bar" style="display:  none">
-                                <div class="col-md-12">
-                                    <div class="progress">
-                                        <div class="bar"></div >
-                                        <div class="percent">0%</div >
-                                    </div>
-                                </div>
+                            <!--<div class="row progress_bar" style="display:  none">-->
+                            <!--<div class="col-md-12">-->
+                            <div id="progressbox" style="display:none;">
+                                <div id="progressbar"></div>
+                                <div id="statustxt">0%</div>
                             </div>
+                            <!--</div>-->
+                            <!--</div>-->
                             <div class="row">
                                 <div class="col-md-12">
                                     <button id="save" type="submit" class="btn btn-primary">Save</button>
@@ -133,37 +153,99 @@
             $('#save').trigger('click');
         });
 
+        var progressbox = $('#progressbox');
+        var progressbar = $('#progressbar');
+
+        var options = {
+            target: '#output', // target element(s) to be updated with server response 
+            beforeSubmit: beforeSubmit, // pre-submit callback 
+            uploadProgress: OnProgress,
+            success: afterSuccess, // post-submit callback 
+            resetForm: true        // reset the form after successful submit 
+        };
+
         $('#mediaForm').submit(function () {
             $('button.save').prop('disabled', true);
 //            $('.media .overlay').show();
 //            $('.media .loading-img').show();
-
-            $('.progress_bar').show();
-            var bar = $('.bar');
-            var percent = $('.percent');
-
-            $('form#mediaForm').ajaxForm({
-                beforeSend: function () {
-                    status.empty();
-                    var percentVal = '0%';
-                    bar.width(percentVal);
-                    percent.html(percentVal);
-                },
-                uploadProgress: function (event, position, total, percentComplete) {
-                    var percentVal = percentComplete + '%';
-                    bar.width(percentVal);
-                    percent.html(percentVal);
-                    //console.log(percentVal, position, total);
-                },
-                success: function () {
-                    var percentVal = '100%';
-                    bar.width(percentVal);
-                    percent.html(percentVal);
-                },
-                complete: function (xhr) {
-                    status.html(xhr.responseText);
-                }
-            });
+//            $('.progress_bar').show();
+            $(this).ajaxSubmit(options);
+            // return false to prevent standard browser submit and page navigation 
         });
+        function OnProgress(event, position, total, percentComplete)
+        {
+            //Progress bar
+            progressbar.width(percentComplete + '%') //update progressbar percent complete
+            statustxt.html(percentComplete + '%'); //update status text
+            if (percentComplete > 50)
+            {
+                statustxt.css('color', '#fff'); //change status text to white after 50%
+            }
+        }
+        function afterSuccess()
+        {
+//            $('#submit-btn').show(); //hide submit button
+//            $('#loading-img').hide(); //hide submit button
+        }
+        function beforeSubmit() {
+            //check whether browser fully supports all File API
+            if (window.File && window.FileReader && window.FileList && window.Blob)
+            {
+
+//                if (!$('#imageInput').val()) //check empty input filed
+//                {
+//                    $("#output").html("Are you kidding me?");
+//                    return false
+//                }
+
+                var fsize = $('#imageInput')[0].files[0].size; //get file size
+                var ftype = $('#imageInput')[0].files[0].type; // get file type
+
+                //allow only valid image file types 
+//                switch (ftype)
+//                {
+//                    case 'image/png':
+//                    case 'image/gif':
+//                    case 'image/jpeg':
+//                    case 'image/pjpeg':
+//                        break;
+//                    default:
+//                        $("#output").html("<b>" + ftype + "</b> Unsupported file type!");
+//                        return false
+//                }
+
+                //Allowed file size is less than 1 MB (1048576)
+//                if (fsize > 1048576)
+//                {
+//                    $("#output").html("<b>" + bytesToSize(fsize) + "</b> Too big Image file! <br />Please reduce the size of your photo using an image editor.");
+//                    return false
+//                }
+
+                //Progress bar
+                progressbox.show(); //show progressbar
+                progressbar.width(completed); //initial value 0% of progressbar
+                statustxt.html(completed); //set status text
+                statustxt.css('color', '#000'); //initial color of status text
+
+
+//                $('#submit-btn').hide(); //hide submit button
+//                $('#loading-img').show(); //hide submit button
+//                $("#output").html("");
+            }
+            else
+            {
+                //Output error to older unsupported browsers that doesn't support HTML5 File API
+//                $("#output").html("Please upgrade your browser, because your current browser lacks some new features we need!");
+//                return false;
+            }
+            //function to format bites bit.ly/19yoIPO
+            function bytesToSize(bytes) {
+                var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+                if (bytes == 0)
+                    return '0 Bytes';
+                var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+                return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+            }
+        }
     });
 </script>
