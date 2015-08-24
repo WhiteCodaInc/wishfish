054456e52704bb4c25ef485472c5c11fea849976
WhiteCodaInc <notifications@whitecoda.com>
2015-08-24 17:32:30 +0530

    update

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -81,7 +81,7 @@
                                                 <li style="margin: 0"><a href="#facilities" data-toggle="tab">STEP 3</a></li>
                                             </ul>
                                             <div class="tab-content">
-                                                <div class="tab-pane" id="location">
+                                                <div class="tab-pane" id="step1">
                                                     <h1 class="info-text">Add a Friend / Family Member :</h1>
                                                     <div class="row">
                                                         <div class="col-md-1"></div>
@@ -124,22 +124,67 @@
                                                                 </a>
                                                             </div>
                                                         </div>
-<!--                                                        <div class="col-md-4" style="padding-top: 60px;">
-                                                            <input style="width: 160px;height: 60px;" type='button' class='btn btn-next btn-info btn-lg' name='next' value='Next' />
-                                                        </div>-->
                                                         <div class="col-md-1"></div>
                                                     </div>
                                                     <input value="" name="zodiac" type="hidden" class="form-control" >
                                                     <input value="" name="age" type="hidden" class="form-control" >
                                                 </div>
-                                                <div class="tab-pane" id="type">
-                                                    <h1 class="info-text">STEP - 2</h1>
+                                                <div class="tab-pane" id="step2">
+                                                    <h1 class="info-text">Verify Your Phone Number:</h1>
+                                                    <p style="text-align: center">
+                                                        Please verify your phone number so that when it is First Name's  birthday,<br/>
+                                                        Wish-Fish can send you a reminder.
+                                                    </p>
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <div class="form-group">
+                                                                <div class="row">
+                                                                    <div class="col-sm-4">
+                                                                        <label>Country Code</label>
+                                                                        <select name="code" class="form-control">
+                                                                            <option value="+1">+1</option>
+                                                                        </select>
+                                                                    </div>
+                                                                    <div class="col-sm-5">
+                                                                        <label>Phone Number</label>
+                                                                        <div class="input-group">
+                                                                            <div class="input-group-addon">
+                                                                                <i class="fa fa-phone"></i>
+                                                                            </div>
+                                                                            <input autofocus="" id="varify_phone" name="phone" type="text" class="form-control"  placeholder="Phone Number" data-inputmask='"mask": "(999) 999-9999"' data-mask/>
+                                                                        </div><!-- /.input group -->
+
+                                                                    </div>
+                                                                    <div style="padding: 5px" class="col-sm-3">
+                                                                        <br/>
+                                                                        <button id="sendcode" class="btn btn-success">Verify</button>
+                                                                    </div>
+                                                                </div>
+                                                            </div><!-- /.form group -->
+                                                        </div>
+                                                    </div>
+                                                    <div id="verifyRow" style="display: none;margin-bottom: 15px" class="row">
+                                                        <div class="col-sm-4" style="  margin-top: 6px;">
+                                                            <label>Verification Code</label>
+                                                        </div>
+                                                        <div class="col-sm-5">
+                                                            <input maxlength="6" name="verifycode" type="text" class="form-control"  placeholder="Verification Code" />
+                                                        </div>
+                                                    </div>
+                                                    <div id="loadRow" style="display: none;margin-bottom: 15px" class="row">
+                                                        <div class="col-md-12">
+                                                            <img class="load" src="<?= base_url() ?>assets/dashboard/img/load.GIF" alt=""  />
+                                                            <span style="display: none;" class="msg"></span>
+                                                        </div>
+                                                    </div>
+                                                    <div id="submitRow" style="display: none" class="row m-bot15">
+                                                        <div class="col-md-3">
+                                                            <button type="button" id="code_submit" class="btn btn-primary pull-left">Submit</button>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="tab-pane" id="facilities">
+                                                <div class="tab-pane" id="step3">
                                                     <h1 class="info-text">STEP - 3</h1>
-                                                </div>
-                                                <div class="tab-pane" id="description">
-                                                    <h1 class="info-text">STEP - 4</h1>
                                                 </div>
                                             </div>
                                             <br/>
@@ -165,6 +210,79 @@
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div>
+    <!------------------------------------------------------------------------>
+    <!-----------------------------   STEP -2 -------------------------------->
+
+    <div class="modal fade" id="nonus-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" style="max-width: 400px">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p>
+                                Unfortunately as of right now we don't support non-US phone numbers (bear with us, we're still a startup!).
+                                However, please <a href="javascript:void(0);" id="feedback" data-toggle="modal" data-target="#feedback-modal">send us an email</a> with your country, and we will let you know as soon as it is available (hopefully soon!)
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12" style="text-align: center">
+                            <button type="button" class="btn btn-danger discard" data-dismiss="modal"><i class="fa fa-times"></i> Discard</button>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+
+    <div class="modal fade" id="feedback-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" style="max-width: 400px">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Feedback / Support</h4>
+                </div>
+                <form id="feedbackForm"  method="post">
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group" >
+                                    <label>Select Country</label>
+                                    <select id="country" class="form-control">
+                                        <option value="-1">--Select Country--</option>
+                                        <option value="India">India</option>
+                                        <option value="Pakistan">Pakistan</option>
+                                        <option value="Canada">Canada</option>
+                                        <option value="Australia">Australia</option>
+                                    </select>
+                                </div>
+                                <div class="form-group" >
+                                    <textarea id="query" class="form-control"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img  src="<?= base_url() ?>assets/dashboard/img/load.GIF" alt="" class="load" style="display: none" />
+                                <span style="display: none" class="msg"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer clearfix">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <button type="button" value="feedback" class="btn btn-primary pull-left send-query">Send</button>
+                            </div>
+                            <div class="col-md-3">
+                                <button type="button" class="btn btn-danger discard" data-dismiss="modal"><i class="fa fa-times"></i> Discard</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+
     <!------------------------------------------------------------------------>
 
     <!--<a href="#" style="display: none" class="feedback" data-backdrop="false"  data-toggle="modal" data-target="#feedback-model">Review</a>-->
@@ -331,6 +449,90 @@
         });
     });
 </script>
+
+<script type="text/javascript">
+    $(function () {
+        $("[data-mask]").inputmask();
+    });
+    $(document).ready(function () {
+
+        $('#feedback').click(function () {
+            $('#feedbackForm span.msg').text('');
+            $('.discard').trigger('click');
+        });
+
+        $('#varify_phone').on("keypress", function (e) {
+            if (e.keyCode == 13) {
+                $('#sendcode').trigger('click');
+            }
+        });
+        $('input[name="verifycode"]').on("keypress", function (e) {
+            if (e.keyCode == 13) {
+                $('#code_submit').trigger('click');
+            }
+        });
+
+        $('#sendcode').click(function () {
+            var phone = $('#varify_phone').val();
+            var code = $('select[name="code"]').val();
+            $('#loadRow').css('display', 'block');
+            $.ajax({
+                type: 'POST',
+                data: {phone: phone, code: code},
+                url: "<?= site_url() ?>app/dashboard/sendVerificationCode",
+                success: function (data, textStatus, jqXHR) {
+                    $('.load').css('display', 'none');
+                    $('.msg').css('display', 'block');
+                    if (data == 1) {
+                        $('.msg').css('color', 'green');
+                        $('.msg').text("Verification Code Successfully Sent To +1" + phone);
+                        $('#verifyRow').css('display', 'block');
+                        $('#submitRow').css('display', 'block');
+                    } else {
+                        $('.msg').css('color', 'red');
+                        $('.msg').text("Invalid Phone Number..!");
+                        $('#verifyRow').css('display', 'none');
+                        $('#submitRow').css('display', 'none');
+                    }
+                }
+            });
+        });
+        $('#code_submit').click(function () {
+            $('.msg').css('display', 'none');
+            $('.load').css('display', 'block');
+            var code = $('input[name="verifycode"]').val();
+            if (!(code.length == 6) || !$.isNumeric(code)) {
+                $('.load').css('display', 'none');
+                $('.msg').css('display', 'block');
+                $('.msg').css('color', 'red');
+                $('.msg').text("Invalid Verification Code..!");
+                return false;
+            }
+            $.ajax({
+                type: 'POST',
+                data: {code: code},
+                url: "<?= site_url() ?>app/dashboard/checkVerificationCode",
+                success: function (data, textStatus, jqXHR) {
+                    if (data == 1) {
+                        $('.close').trigger('click');
+                        alertify.success("Congratulations! You have verified your phone number successfully!");
+                        setTimeout(function () {
+                            location.reload(true);
+                        }, 1000);
+                    } else {
+                        $('.load').css('display', 'none');
+                        $('.msg').css('display', 'block');
+                        $('.msg').css('color', 'red');
+                        $('.msg').text("Invalid Verification Code..!");
+                    }
+                }
+            });
+        });
+    });
+</script>
+
+
+
 
 <!--   plugins 	 -->
 <script src="<?= base_url() ?>assets/dashboard/js/plugins/form-wizard/jquery.bootstrap.wizard.js" type="text/javascript"></script>
