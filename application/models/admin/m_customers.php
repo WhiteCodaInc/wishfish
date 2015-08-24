0532d68fb00cef66c60a4f6b64d2a9dd74ff3e75
WhiteCodaInc <notifications@whitecoda.com>
2015-08-24 09:47:27 +0530

    update

--- application/models/admin/m_customers.php
+++ application/models/admin/m_customers.php
@@ -59,7 +59,7 @@ class M_customers extends CI_Model {
                 $where['is_lifetime'] = 1;
             } else {
                 $where['P.plan_id'] = $plan;
-                $where['is_lifetime <>'] = 1;
+                
             }
         }
 
