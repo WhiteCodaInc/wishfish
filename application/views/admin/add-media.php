7051c6f22e39926fe2e247f098868b75b7111f56
WhiteCodaInc <notifications@whitecoda.com>
2015-08-24 13:32:28 +0530

    update

--- application/views/admin/add-media.php
+++ application/views/admin/add-media.php
@@ -189,8 +189,7 @@
         }
         function afterSuccess()
         {
-//            $('#submit-btn').show(); //hide submit button
-//            $('#loading-img').hide(); //hide submit button
+            windows.location.href = "<?= site_url() . 'admin/media?msg=I' ?>";
         }
         function beforeSubmit() {
             //check whether browser fully supports all File API
