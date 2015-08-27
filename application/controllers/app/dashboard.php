77540402dcdb42ee7aa0c06eec97458769254739
WhiteCodaInc <notifications@whitecoda.com>
2015-08-27 09:56:26 +0530

    update

--- application/controllers/app/dashboard.php
+++ application/controllers/app/dashboard.php
@@ -262,13 +262,11 @@ class Dashboard extends CI_Controller {
                 $ev[$key]['icon'] = "square_blue.png";
                 if ($value->notify == "them") {
                     $cInfo = $this->wi_common->getContactInfo($value->contact_id);
-                    print_r($cInfo);
                     $img_src = ($cInfo->contact_avatar != "") ?
                             "http://mikhailkuznetsov.s3.amazonaws.com/" . $value->contact_avatar :
                             base_url() . 'assets/dashboard/img/default-avatar.png';
                 } else {
                     $uInfo = $this->wi_common->getUserInfo($value->user_id);
-                    print_r($uInfo);
                     $img_src = ($uInfo->profile_pic != "") ?
                             "http://mikhailkuznetsov.s3.amazonaws.com/" . $value->profile_pic :
                             base_url() . 'assets/dashboard/img/default-avatar.png';
