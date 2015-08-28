93dbfed491bba2a874f714b75ecb67fb71a7ba90
WhiteCodaInc <notifications@whitecoda.com>
2015-08-28 16:22:13 +0530

    update

--- application/models/admin/m_customers.php
+++ application/models/admin/m_customers.php
@@ -27,7 +27,7 @@ class M_customers extends CI_Model {
 
     function getCustomerDetail() {
         $this->db->select('U.user_id,profile_pic,name,email,phone,phone_verification,P.plan_name,PD.plan_id,status,plan_status,cancel_by,is_lifetime');
-        $this->db->select_max('PD.register_date', 'r_date');
+        $this->db->select_max('PD.register_date', 'register_date');
         $this->db->from('wi_user_mst as U');
         $this->db->join('wi_plan_detail as PD', 'U.user_id = PD.user_id', 'left outer');
         $this->db->join('wi_plans as P', 'PD.plan_id = P.plan_id');
