4ffd3b8603f09efae35f202bacdbd79b16328a48
WhiteCodaInc <notifications@whitecoda.com>
2015-08-28 16:19:52 +0530

    update

--- application/models/admin/m_customers.php
+++ application/models/admin/m_customers.php
@@ -36,10 +36,10 @@ class M_customers extends CI_Model {
 //        $this->db->order_by('name', 'asc');
         $this->db->group_by('PD.user_id');
         $query = $this->db->get();
-        echo '<pre>';
-        print_r($query->result());
-        die();
-//        return $query->result();
+//        echo '<pre>';
+//        print_r($query->result());
+//        die();
+        return $query->result();
     }
 
     function searchResult() {
