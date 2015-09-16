6c38634114a3d52c38c077a98eb66b9a21f7640f
WhiteCodaInc <notifications@whitecoda.com>
2015-09-16 11:43:58 +0530

    update

--- application/models/admin/m_email_list.php
+++ application/models/admin/m_email_list.php
@@ -23,7 +23,7 @@ class M_email_list extends CI_Model {
     function getEmailLists() {
         $this->db->select("E.name,count(E.list_id) as totalE,count(contact_id) as totalC");
         $this->db->from('email_list as E');
-        $this->db->join('email_list_contacts as EC', 'E.list_id = EC.list_id');
+        $this->db->join('email_list_contacts as EC', 'E.list_id = EC.list_id','left outer');
         $query = $this->db->get();
         echo '<pre>';
         print_r($query->result());
