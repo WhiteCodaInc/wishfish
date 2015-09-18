813b0ccc618a38516b74f23286a324e0c8661ffd
WhiteCodaInc <notifications@whitecoda.com>
2015-09-18 15:24:47 +0530

    update

--- application/views/admin/admin_navbar.php
+++ application/views/admin/admin_navbar.php
@@ -68,107 +68,151 @@ $img_src = ($avatar != "") ?
                         </ul>
                     </li>
                 <?php endif; ?>
-<!--                <li class="treeview" id="11">
-                    <a style="float: right" href="#">
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <a  href="<?= site_url() ?>admin/faq">
-                        <i class="fa fa-angle-double-right"></i>
-                        <span>FAQ'S</span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li>
-                            <a href="<?= site_url() ?>admin/faq/faqCategory">
-                                <i class="fa fa-angle-double-right"></i> <span>FAQ'S Category</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="treeview" id="12">
-                    <a style="float: right" href="#">
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <a  href="<?= site_url() ?>admin/pages">
-                        <i class="fa fa-angle-double-right"></i>
-                        <span>Web Pages</span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li>
-                            <a href="<?= site_url() ?>admin/sections/">
-                                <i class="fa fa-angle-double-right"></i> <span>Homepage Section</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="" id="13">
-                    <a href="<?= site_url() ?>admin/feedback">
-                        <i class="fa fa-angle-double-right"></i> <span>Feedback/Support</span>
-                    </a>
-                </li>
-                <li class="" id="14">
-                    <a href="<?= site_url() ?>admin/coupons">
-                        <i class="fa fa-angle-double-right"></i> <span>Coupons</span>
-                    </a>
-                </li>
-                <li class="treeview" id="15">
-                    <a href="#">
-                        <i class="fa fa-wrench"></i>
-                        <span>Analytics</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li>
-                            <a href="<?= site_url() ?>admin/analytics">
-                                <i class="fa fa-angle-double-right"></i> <span>Total Payment</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= site_url() ?>admin/analytics/totalUser">
-                                <i class="fa fa-angle-double-right"></i> <span>Total Users</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= site_url() ?>admin/analytics/totalNewUser">
-                                <i class="fa fa-angle-double-right"></i> <span>Total New User</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= site_url() ?>admin/analytics/adminReport">
-                                <i class="fa fa-angle-double-right"></i> <span>Admin Report</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="" id="16">
-                    <a href="<?= site_url() ?>admin/products">
-                        <i class="fa fa-angle-double-right"></i> <span>Product Builder</span>
-                    </a>
-                </li>
-                <li class="" id="17">
-                    <a href="<?= site_url() ?>admin/plans">
-                        <i class="fa fa-angle-double-right"></i> <span>Payment Plan Builder</span>
-                    </a>
-                </li>
-                <li class="" id="18">
-                    <a href="<?= site_url() ?>admin/offers">
-                        <i class="fa fa-angle-double-right"></i> <span>Offer Builder</span>
-                    </a>
-                </li>
-                <li class="" id="19">
-                    <a href="<?= site_url() ?>page-builder/" target="_blank">
-                        <i class="fa fa-angle-double-right"></i> <span>Page Builder</span>
-                    </a>
-                </li>
-                <li class="" id="20">
-                    <a href="<?= site_url() ?>admin/media/">
-                        <i class="fa fa-angle-double-right"></i> <span>Media Library</span>
-                    </a>
-                </li>
-                <li class="" id="21">
-                    <a href="<?= site_url() ?>admin/email_list">
-                        <i class="fa fa-angle-double-right"></i> <span>Funnel Email List</span>
-                    </a>
-                </li>-->
+                <?php
+                if (
+                        (isset($rule->coni) && $rule->coni) ||
+                        (isset($rule->conu) && $rule->conu) ||
+                        (isset($rule->cond) && $rule->cond) ||
+                        (isset($rule->congi) && $rule->congi) ||
+                        (isset($rule->congu) && $rule->congu) ||
+                        (isset($rule->congd) && $rule->congd) ||
+                        (isset($rule->cbl) && $rule->cbl)):
+                    ?>
+                    <li class="treeview" id="4">
+                        <a style="float: right" href="#">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <a href="<?= site_url() ?>admin/contacts">
+                            <i class="fa fa-wrench"></i> <span>Company Contacts</span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li>
+                                <a href="<?= site_url() ?>admin/contacts/addContact">
+                                    <i class="fa fa-plus"></i>
+                                    <span>Create New Contact</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= site_url() ?>admin/contact_groups">
+                                    <i class="fa fa-angle-double-right"></i> <span>Contact Groups</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= site_url() ?>admin/contacts/block_list">
+                                    <i class="fa fa-lock"></i>
+                                    <span>Contact Block List</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= site_url() ?>admin/scrape">
+                                    <i class="fa fa-angle-double-right"></i>
+                                    <span>Scrape Contacts</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                <?php endif; ?>
+                <!--                <li class="treeview" id="11">
+                                    <a style="float: right" href="#">
+                                        <i class="fa fa-angle-left pull-right"></i>
+                                    </a>
+                                    <a  href="<?= site_url() ?>admin/faq">
+                                        <i class="fa fa-angle-double-right"></i>
+                                        <span>FAQ'S</span>
+                                    </a>
+                                    <ul class="treeview-menu">
+                                        <li>
+                                            <a href="<?= site_url() ?>admin/faq/faqCategory">
+                                                <i class="fa fa-angle-double-right"></i> <span>FAQ'S Category</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="treeview" id="12">
+                                    <a style="float: right" href="#">
+                                        <i class="fa fa-angle-left pull-right"></i>
+                                    </a>
+                                    <a  href="<?= site_url() ?>admin/pages">
+                                        <i class="fa fa-angle-double-right"></i>
+                                        <span>Web Pages</span>
+                                    </a>
+                                    <ul class="treeview-menu">
+                                        <li>
+                                            <a href="<?= site_url() ?>admin/sections/">
+                                                <i class="fa fa-angle-double-right"></i> <span>Homepage Section</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="" id="13">
+                                    <a href="<?= site_url() ?>admin/feedback">
+                                        <i class="fa fa-angle-double-right"></i> <span>Feedback/Support</span>
+                                    </a>
+                                </li>
+                                <li class="" id="14">
+                                    <a href="<?= site_url() ?>admin/coupons">
+                                        <i class="fa fa-angle-double-right"></i> <span>Coupons</span>
+                                    </a>
+                                </li>
+                                <li class="treeview" id="15">
+                                    <a href="#">
+                                        <i class="fa fa-wrench"></i>
+                                        <span>Analytics</span>
+                                        <i class="fa fa-angle-left pull-right"></i>
+                                    </a>
+                                    <ul class="treeview-menu">
+                                        <li>
+                                            <a href="<?= site_url() ?>admin/analytics">
+                                                <i class="fa fa-angle-double-right"></i> <span>Total Payment</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="<?= site_url() ?>admin/analytics/totalUser">
+                                                <i class="fa fa-angle-double-right"></i> <span>Total Users</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="<?= site_url() ?>admin/analytics/totalNewUser">
+                                                <i class="fa fa-angle-double-right"></i> <span>Total New User</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="<?= site_url() ?>admin/analytics/adminReport">
+                                                <i class="fa fa-angle-double-right"></i> <span>Admin Report</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="" id="16">
+                                    <a href="<?= site_url() ?>admin/products">
+                                        <i class="fa fa-angle-double-right"></i> <span>Product Builder</span>
+                                    </a>
+                                </li>
+                                <li class="" id="17">
+                                    <a href="<?= site_url() ?>admin/plans">
+                                        <i class="fa fa-angle-double-right"></i> <span>Payment Plan Builder</span>
+                                    </a>
+                                </li>
+                                <li class="" id="18">
+                                    <a href="<?= site_url() ?>admin/offers">
+                                        <i class="fa fa-angle-double-right"></i> <span>Offer Builder</span>
+                                    </a>
+                                </li>
+                                <li class="" id="19">
+                                    <a href="<?= site_url() ?>page-builder/" target="_blank">
+                                        <i class="fa fa-angle-double-right"></i> <span>Page Builder</span>
+                                    </a>
+                                </li>
+                                <li class="" id="20">
+                                    <a href="<?= site_url() ?>admin/media/">
+                                        <i class="fa fa-angle-double-right"></i> <span>Media Library</span>
+                                    </a>
+                                </li>
+                                <li class="" id="21">
+                                    <a href="<?= site_url() ?>admin/email_list">
+                                        <i class="fa fa-angle-double-right"></i> <span>Funnel Email List</span>
+                                    </a>
+                                </li>-->
             </ul>
             <script type="text/javascript">
                 $(document).ready(function () {
