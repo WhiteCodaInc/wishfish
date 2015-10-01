b30e8ecca52cefba7cd2a200cedb1c629ede944d
WhiteCodaInc <notifications@whitecoda.com>
2015-10-01 13:45:39 +0530

    update

--- application/views/affiliate/dummy-dashboard.php
+++ application/views/affiliate/dummy-dashboard.php
@@ -114,6 +114,129 @@
     ?>
     <body class="skin-blue">
         <div class="wrapper row-offcanvas row-offcanvas-left">
+
+            <header class="header">
+                <a href="<?= site_url() ?>admin/dashboard" class="logo">
+                    <!--Add the class icon to your logo image or logo icon to add the margining--> 
+                    <?= (!$affInfo) ? $this->session->userdata('d-name') : $affInfo->fname . ' ' . $affInfo->lname ?> 
+                </a>
+                <!-- Header Navbar: style can be found in header.less -->
+                <nav class="navbar navbar-static-top fixed"  role="navigation">
+                    <!-- Sidebar toggle button-->
+                    <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <div class="navbar-right">
+                        <ul class="nav navbar-nav">
+                            <!-- New Payment Notification -->
+                            <li class="dropdown messages-menu payment-notification">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="fa fa-tasks"></i>
+                                    <span class="label label-success">
+                                        0
+                                    </span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="header">You have 0 payments</li>
+                                    <li>
+                                        <!-- inner menu: contains the actual data -->
+                                        <ul class="menu">
+
+                                        </ul>
+                                    </li>
+                                    <li class="footer"><a href="#">See All Payments</a></li>
+                                </ul>
+                            </li>
+                            <!-- ENd Payment Notification -->
+                            <!-- New Customer Join Notification -->
+                            <li class="dropdown messages-menu customer-notification">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="fa fa-tasks"></i>
+                                    <span class="label label-success">
+                                        0
+                                    </span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="header">You have 0 Customers</li>
+                                    <li>
+                                        <!-- inner menu: contains the actual data -->
+                                        <ul class="menu newCustomer">
+
+                                        </ul>
+                                    </li>
+                                    <li class="footer"><a href="#">See All Customers</a></li>
+                                </ul>
+                            </li>
+                            <!-- ENd New Customer Join Notification -->
+                            <!-- SMS Inbox -->
+                            <li class="dropdown messages-menu sms-notification">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="fa fa-envelope"></i>
+                                    <span class="label label-success">
+                                        0
+                                    </span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="header">You have 0 messages</li>
+                                    <li>
+                                        <!-- inner menu: contains the actual data -->
+                                        <ul class="menu unreadSMS">
+
+                                        </ul>
+                                    </li>
+                                    <li class="footer"><a href="#">See All Messages</a></li>
+                                </ul>
+                            </li>
+                            <!-- End SMS Inbox -->
+                            <!-- Emails -->
+                            <li class="dropdown messages-menu email-notify">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="fa fa-envelope-o"></i>
+                                    <span class="label label-success ebadge">0</span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="header">You have 0 emails</li>
+                                    <li>
+                                        <!-- inner menu: contains the actual data -->
+                                        <ul class="menu"></ul>
+                                    </li>
+                                    <li class="footer"><a href="#">See All Emails</a></li>
+                                </ul>
+                            </li>
+                            <!-- ENd Email -->
+                            <li class="dropdown user user-menu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="glyphicon glyphicon-user"></i>
+                                    <span><i class="caret"></i></span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <!-- User image -->
+                                    <li class="user-header bg-light-blue">
+                                        <img src="<?= $aff_img_src ?>" class="img-circle" alt="User Image" />
+                                        <p>
+                                            <?= (!$affInfo) ? $this->session->userdata('d-name') : $affInfo->fname . ' ' . $affInfo->lname ?>
+                                        </p>
+                                    </li>
+                                    <!-- Menu Body -->
+                                    <!-- Menu Footer-->
+                                    <li class="user-footer">
+                                        <div class="pull-right">
+                                            <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="row title-blue">
+                        <div id="titleblue" class="col-md-12"></div>
+                    </div>
+                </nav>
+            </header>
+
             <aside class="left-side sidebar-offcanvas">
                 <section class="sidebar">
                     <!-- Sidebar user panel -->
@@ -412,127 +535,7 @@
 
             <!-- header logo: style can be found in header.less -->
 
-            <header class="header">
-                <a href="<?= site_url() ?>admin/dashboard" class="logo">
-                    <!--Add the class icon to your logo image or logo icon to add the margining--> 
-                    <?= (!$affInfo) ? $this->session->userdata('d-name') : $affInfo->fname . ' ' . $affInfo->lname ?> 
-                </a>
-                <!-- Header Navbar: style can be found in header.less -->
-                <nav class="navbar navbar-static-top fixed"  role="navigation">
-                    <!-- Sidebar toggle button-->
-                    <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                    <div class="navbar-right">
-                        <ul class="nav navbar-nav">
-                            <!-- New Payment Notification -->
-                            <li class="dropdown messages-menu payment-notification">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-tasks"></i>
-                                    <span class="label label-success">
-                                        0
-                                    </span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 0 payments</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu">
 
-                                        </ul>
-                                    </li>
-                                    <li class="footer"><a href="#">See All Payments</a></li>
-                                </ul>
-                            </li>
-                            <!-- ENd Payment Notification -->
-                            <!-- New Customer Join Notification -->
-                            <li class="dropdown messages-menu customer-notification">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-tasks"></i>
-                                    <span class="label label-success">
-                                        0
-                                    </span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 0 Customers</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu newCustomer">
-
-                                        </ul>
-                                    </li>
-                                    <li class="footer"><a href="#">See All Customers</a></li>
-                                </ul>
-                            </li>
-                            <!-- ENd New Customer Join Notification -->
-                            <!-- SMS Inbox -->
-                            <li class="dropdown messages-menu sms-notification">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-envelope"></i>
-                                    <span class="label label-success">
-                                        0
-                                    </span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 0 messages</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu unreadSMS">
-
-                                        </ul>
-                                    </li>
-                                    <li class="footer"><a href="#">See All Messages</a></li>
-                                </ul>
-                            </li>
-                            <!-- End SMS Inbox -->
-                            <!-- Emails -->
-                            <li class="dropdown messages-menu email-notify">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-envelope-o"></i>
-                                    <span class="label label-success ebadge">0</span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 0 emails</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu"></ul>
-                                    </li>
-                                    <li class="footer"><a href="#">See All Emails</a></li>
-                                </ul>
-                            </li>
-                            <!-- ENd Email -->
-                            <li class="dropdown user user-menu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="glyphicon glyphicon-user"></i>
-                                    <span><i class="caret"></i></span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <!-- User image -->
-                                    <li class="user-header bg-light-blue">
-                                        <img src="<?= $aff_img_src ?>" class="img-circle" alt="User Image" />
-                                        <p>
-                                            <?= (!$affInfo) ? $this->session->userdata('d-name') : $affInfo->fname . ' ' . $affInfo->lname ?>
-                                        </p>
-                                    </li>
-                                    <!-- Menu Body -->
-                                    <!-- Menu Footer-->
-                                    <li class="user-footer">
-                                        <div class="pull-right">
-                                            <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="row title-blue">
-                        <div id="titleblue" class="col-md-12"></div>
-                    </div>
-                </nav>
-            </header>
             <aside class="right-side">
                 <!-- Content Header (Page header) -->
                 <section class="content-header" style="display: none">
