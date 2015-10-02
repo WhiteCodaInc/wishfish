2b7031bc6fab4fe7976c76f9e2c1113f75fafb4e
WhiteCodaInc <notifications@whitecoda.com>
2015-10-02 11:14:11 +0530

    update

--- application/models/admin/m_affiliates.php
+++ application/models/admin/m_affiliates.php
@@ -248,8 +248,8 @@ class M_affiliates extends CI_Model {
         if ($post['payouttype'] == "global") {
             $set = array(
                 'payout_type' => 1,
-                'normal_payout' => $row['normal'],
-                'recurring_payout' => $row['recurring']
+                'normal_payout' => $row->normal,
+                'recurring_payout' => $row->recurring
             );
         } else {
             $set = array(
@@ -258,7 +258,8 @@ class M_affiliates extends CI_Model {
                 'recurring_payout' => $post['recurring']
             );
         }
-        $this->db->where('affiliate_id IN (' . implode(',', $post['affiliate']) . ')');
+//        $this->db->where('affiliate_id IN (' . implode(',', $post['affiliate']) . ')');
+        $this->db->where_in('affiliate_id', $post['affiliate']);
         $this->db->update('affiliate_detail', $set);
         echo '1';
     }
