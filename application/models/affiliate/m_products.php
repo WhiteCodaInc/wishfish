a057221c0bd9c45702e6d96cbcbb5a1155f432cb
WhiteCodaInc <notifications@whitecoda.com>
2015-10-05 15:44:48 +0530

    update

--- application/models/affiliate/m_products.php
+++ application/models/affiliate/m_products.php
@@ -13,12 +13,25 @@
  */
 class M_products extends CI_Model {
 
+    private $affid;
+
     function __construct() {
         parent::__construct();
+        $this->affid = $this->session->userdata('a_affid');
     }
 
     function getProductDetail() {
-        $query = $this->db->get('promote_products');
+        $this->db->select('*');
+        $this->db->distinct('M.offer_id');
+        $this->db->from('multiple_affiliate_offer as M');
+        $this->db->join('affiliate_offers as O', 'M.offer_id = O.offer_id', 'left outer');
+        $this->db->join('products as P', 'O.product_id = P.product_id', 'left outer');
+        $this->db->join('payment_plan as PL', 'O.payment_plan_id = PL.payment_plan_id', 'left outer');
+        $this->db->where('M.affiliate_id', $this->affid);
+        $query = $this->db->get();
+        echo '<pre>';
+        print_r($query->result());
+        die();
         return $query->result();
     }
 
