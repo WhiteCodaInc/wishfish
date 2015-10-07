a0672d72bfe3ccbeae69c8139715eb4dd1e3a410
WhiteCodaInc <notifications@whitecoda.com>
2015-10-07 12:14:46 +0530

    update

--- application/views/admin/affiliate-detail.php
+++ application/views/admin/affiliate-detail.php
@@ -463,7 +463,7 @@ switch ($msg) {
                 var aid = $(this).attr('data-aff_id');
                 var pid = $(this).attr('data-payout_id');
 
-                $('#payoutForm input:radio:nth(' + pid + ')').prop('checked', true);
+                $('#payoutForm input:radio:nth(' + pid - 1 + ')').prop('checked', true);
 
                 var title = (pid == '1') ?
                         "GLOBAL" : ((pid == '2') ? "AFFILIATE SPECIFIC" : "OFFER SPECIFIC");
